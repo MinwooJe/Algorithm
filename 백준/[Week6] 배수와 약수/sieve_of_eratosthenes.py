ab1cbdad679883ffae806b95c2a4d0b9a81eb687
MinwooJe <gpatration77@gmail.com>
2024-04-03 10:07:14 +0900

    [feat]: 6주차 문제풀이 추가

--- 백준/[Week6] 배수와 약수/sieve_of_eratosthenes.py
+++ 백준/[Week6] 배수와 약수/sieve_of_eratosthenes.py
@@ -1,7 +1,3 @@
-n = int(input())
-prime_list = []
-
-# 에라토스테네스의 체
 def sieve_of_eratosthenes(a):
     numbers = [x for x in range(a+1)]
     end = int(a**(1/2))
@@ -11,13 +7,3 @@ def sieve_of_eratosthenes(a):
         for j in range(i+i, a+1, i):    # 자기 자신을 제외한 i의 배수는 모두 0으로 처리함.
             numbers[j] = 0
     return [i for i in numbers[2:] if numbers[i]]
-# 소인수분해
-
-prime = sieve_of_eratosthenes(n)
-result = []
-
-for i in prime:
-    if n % i == 0:
-        while n % i == 0:
-            n = n // i
-            print(i)
