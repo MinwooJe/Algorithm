19c0992deb8278f95344f8bde2bba465aa2c9d8a
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-04-18 16:58:21 +0900

    [level 0] Title: 문자열 묶기, Time: 34.11 ms, Memory: 17.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
@@ -0,0 +1,10 @@
+def solution(strArr):
+    answer = 0
+    cntDict = dict()
+    for s in strArr:
+        if len(s) in cntDict.keys():
+            cntDict[len(s)] += 1
+        else:
+            cntDict[len(s)] = 1
+    answer = max(cntDict.values()) 
+    return answer
