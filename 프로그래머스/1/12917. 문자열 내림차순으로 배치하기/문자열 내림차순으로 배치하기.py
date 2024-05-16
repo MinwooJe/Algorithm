23472f6ca9c4038db68b3a7faccd955add2bc750
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-05-16 14:57:21 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.py
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.py
@@ -1,11 +1,3 @@
 def solution(s):
-    s_list = []
-    answer = ''
-    
-    for i in s:
-        s_list.append(i)
-    
-    s_list.sort(reverse=True)
-    answer = ''.join(s_list)
-        
+    answer = ''.join(sorted(s, reverse=True))
     return answer
