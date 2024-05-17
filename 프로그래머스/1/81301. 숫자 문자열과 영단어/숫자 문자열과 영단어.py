2dcff0c080e57c546e8acba101c23a956d3b9214
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-05-17 22:48:36 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,10 +1,8 @@
 def solution(s):
-    answer = 0
-    dic = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7,
-          'eight':8, 'nine':9, 'zero':0}
+    hash = {'zero':0, 'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
     
-    for i in dic:
-        if i in s:
-            s = s.replace(i, str(dic[i]))
-    answer = int(s)
-    return answer
+    for key, value in hash.items():
+        if key in s:
+            s = s.replace(key, str(value))
+    
+    return int(s)
