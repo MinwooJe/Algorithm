b3d02228abc929e3c730722bbfd6db1a8ffb72ea
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-05-18 10:44:37 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,19 +1,14 @@
 def solution(n, arr1, arr2):
     answer = []
-    
-    # 비트연산을 통해 최종 벽을 만들기
     for i in range(n):
-        answer.append(bin(arr1[i] | arr2[i])[2:])
-    print(answer)
-    
-    # 길이가 부족한 경우 예외 처리
-    for i in range(len(answer)):
-        len_dif = len(max(answer)) - len(answer[i]) 
-        if len_dif > 0:
-            answer[i] = '0' * len_dif + answer[i]
+        result = bin(arr1[i] | arr2[i])[2:]
+        
+        while len(result) < n:
+            result = '0' + result
             
-    # 1, 0으로 구성된 지도를 #과 공백으로 바꾸기
-    for idx, value in enumerate(answer):
-        answer[idx] = value.replace('1', '#')
-        answer[idx] = answer[idx].replace('0', ' ')
+        result = result.replace('1', '#')
+        result = result.replace('0', ' ')
+        
+        answer.append(result)
+        
     return answer
