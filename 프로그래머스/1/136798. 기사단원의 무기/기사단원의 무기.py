2962b93530df44841bd0b8fc047ce81683d0171a
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-05-24 14:05:40 +0900

    [level 1] Title: 기사단원의 무기, Time: 1.49 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,20 +1,19 @@
 def solution(number, limit, power):
-    answer = 0
+    power_list = count_div(number)
+    for i in range(len(power_list)):
+        if power_list[i] > limit:
+            power_list[i] = power
+    return sum(power_list)
+
+def count_div(number):
     power_list = []
-    
-    for i in range(1, number + 1):
+    for i in range(1, number+1):
         count = 0
-        for j in range(1, int(i ** (1/2)) + 1):
-            if i == j*j:
+        for j in range(1, int(i**(0.5)) + 1):
+            if i % j == 0:
                 count += 1
-            elif i % j == 0:
-                count += 2
+                if i != j*j:
+                    count += 1
         power_list.append(count)
-    
-    for idx, value in enumerate(power_list):
-        if value > limit:
-            power_list[idx] = power
-    
-    answer = sum(power_list)
-        
-    return answer
+    return power_list
+
