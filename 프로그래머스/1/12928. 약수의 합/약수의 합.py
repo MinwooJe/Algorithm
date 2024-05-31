b279151434e65fee4fd65b9df6235862e486344e
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-05-31 23:49:17 +0900

    [level 1] Title: 약수의 합, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,8 +1,10 @@
 def solution(n):
-    answer = [n]
+    answer = set()   # n이 제곱수일때 i == n/i이므로 중복 제거 위해 set 사용
+    answer.add(n)      
     
-    for i in range(1, n//2+1):
+    for i in range(1, int(n**0.5)+1):
         if n % i == 0:
-            answer.append(i)
+            answer.add(i)
+            answer.add(n/i) 
             
     return sum(answer)
