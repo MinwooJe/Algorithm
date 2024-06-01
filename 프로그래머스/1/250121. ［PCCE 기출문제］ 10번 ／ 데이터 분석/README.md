f49d59a2b27a939d3be906dadf48de080a6acf4c
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-06-01 17:02:44 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.16 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/README.md
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/README.md
@@ -1,10 +1,10 @@
 # [level 1] [PCCE 기출문제] 10번 / 데이터 분석 - 250121 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250121#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250121) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 10.09 ms
+메모리: 10.2 MB, 시간: 0.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 10일 09:38:25
+2024년 06월 01일 17:02:42
 
 ### 문제 설명
 
--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -1,28 +1,13 @@
-def get_value(s):
-    if s == 'code':
-        return 0
-    elif s == 'date':
-        return 1
-    elif s == 'maximum':
-        return 2
-    else:
-        return 3
-
-def solution(data, ext, val_ext, sort_by):
-    answer = []
-    max = 0
+def solution(data, ext, val_ext, sort_by):    
+    ext_dict = {'code':0, 'date':1, 'maximum':2, 'remain':3}
+    filtered_data = filtering(data, ext, val_ext)
+    sorted_data = sorted(filtered_data, key=lambda x: x[ext_dict[sort_by]])
+    return sorted_data
     
-    for arr in data:
-        if arr[get_value(ext)] < val_ext:
-            answer.append(arr)
-    
-    
-    criteria = get_value(sort_by)
-    for n in range(len(answer) - 1, 0, -1):
-        for i in range(n):
-            if answer[i][criteria] > answer[i + 1][criteria]:
-                temp = answer[i]
-                answer[i] = answer[i+1]
-                answer[i+1] = temp            
-    
-    return answer
+def filtering(data, ext, val_ext):
+    ext_dict = {'code':0, 'date':1, 'maximum':2, 'remain':3}
+    filtered_data = []
+    for d in data:
+        if d[ext_dict[ext]] < val_ext:
+            filtered_data.append(d)
+    return filtered_data
