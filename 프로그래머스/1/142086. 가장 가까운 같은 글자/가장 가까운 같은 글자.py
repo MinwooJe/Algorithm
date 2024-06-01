8bea8ef8d14206a44758478b36a0ded7737e5a2b
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-06-01 16:07:03 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.43 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,11 +1,10 @@
 def solution(s):
+    latest_char_idx = {}
     answer = []
-    char_dict = {}   # 가장 최근에 나온 문자의 인덱스를 저장하는 딕셔너리
-    
-    for i in range(len(s)):
-        if s[i] not in char_dict:
-            answer.append(-1)
+    for idx, char in enumerate(s):
+        if char in latest_char_idx:
+            answer.append(idx - latest_char_idx[char])
         else:
-            answer.append(i - char_dict[s[i]])
-        char_dict[s[i]] = i
+            answer.append(-1)
+        latest_char_idx[char] = idx
     return answer
