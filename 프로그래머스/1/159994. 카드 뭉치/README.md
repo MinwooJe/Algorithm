89e116756fc351ccdf1612e6bfe7dbad2339ba7f
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-06-02 22:41:19 +0900

    [level 1] Title: 카드 뭉치, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/README.md
+++ 프로그래머스/1/159994. 카드 뭉치/README.md
@@ -1,10 +1,10 @@
 # [level 1] 카드 뭉치 - 159994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.00 ms
+메모리: 10.2 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 19일 13:58:23
+2024년 06월 02일 22:41:17
 
 ### 문제 설명
 
--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,13 +1,10 @@
 def solution(cards1, cards2, goal):
-    answer = 'Yes'
-    p1, p2 = 0, 0   # cards1,2의 각 인덱스를 가리키는 포인터
-    
-    for i in range(len(goal)):
-        if p1 < len(cards1) and cards1[p1] == goal[i]:
-            p1 += 1
-        elif p2 < len(cards2) and cards2[p2] == goal[i]:
-            p2 += 1
+    for i in goal:
+        if cards1 and i == cards1[0]:
+            cards1.pop(0)
+        elif cards2 and i == cards2[0]:
+            cards2.pop(0)
         else:
-            answer = 'No'
-            break
-    return answer
+            return 'No'
+        
+    return 'Yes'
