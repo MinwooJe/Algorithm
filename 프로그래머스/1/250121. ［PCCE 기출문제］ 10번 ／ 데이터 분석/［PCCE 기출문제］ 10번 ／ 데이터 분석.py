3e0fb0b1db9cd2dd4b34d1f500f5f27b7fa82611
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-06-02 21:19:58 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.16 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -1,13 +1,13 @@
-def solution(data, ext, val_ext, sort_by):    
-    ext_dict = {'code':0, 'date':1, 'maximum':2, 'remain':3}
-    filtered_data = filtering(data, ext, val_ext)
-    sorted_data = sorted(filtered_data, key=lambda x: x[ext_dict[sort_by]])
-    return sorted_data
-    
-def filtering(data, ext, val_ext):
-    ext_dict = {'code':0, 'date':1, 'maximum':2, 'remain':3}
+def solution(data, ext, val_ext, sort_by):
+    ext_mapping = {'code':0, 'date':1, 'maximum':2, 'remain':3}
+    filtered_data = filtering_data(data, ext, val_ext, ext_mapping)
+    answer = sorted(filtered_data, key=lambda x: x[ext_mapping[sort_by]])
+    return answer
+
+def filtering_data(data, ext, val_ext, ext_mapping):
     filtered_data = []
     for d in data:
-        if d[ext_dict[ext]] < val_ext:
+        if d[ext_mapping[ext]] < val_ext:
             filtered_data.append(d)
     return filtered_data
+    
