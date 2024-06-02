c5866cc720310255b78da7ce1749915702301312
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-06-02 22:33:39 +0900

    [level 1] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -1,14 +1,13 @@
 def solution(players, callings):
-    # 인덱스를 저장하는 딕셔너리
-    players_dict = {player: idx for idx, player in enumerate(players)}
     
+    calling_dict = {p:idx for idx, p in enumerate(players)}
+
     for p in callings:
-        idx = players_dict[p]   # 불린 선수의 순위
+        idx = calling_dict[p]
+        # idx = players.index(p)    -> 인덱스 찾는데 n, for 문 n => n^2
+
+        players[idx-1], players[idx] = players[idx], players[idx-1]
         
-        # 원래 리스트에서 위치 변경
-        players[idx], players[idx-1] = players[idx-1], players[idx]
-        
-        # 딕셔너리에서 선수들의 인덱스(순위) 변경
-        players_dict[players[idx]] += 1     # 추월당한 선수의 순위를 원래 순위에서 +1
-        players_dict[players[idx-1]] -= 1 # 추월한 선수의 순위를 원래 순위에서 -1
+        calling_dict[players[idx]] += 1
+        calling_dict[players[idx-1]] -= 1
     return players
