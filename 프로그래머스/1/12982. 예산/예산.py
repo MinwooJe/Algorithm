34d7029ce450c29d8493071a7ae7587150e8e65b
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-06-03 15:43:14 +0900

    [level 1] Title: 예산, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,13 +1,7 @@
 def solution(d, budget):
     d.sort()
-    answer = 0
     i = 0
-
-    # 인덱스 조건을 먼저 검사하지 않으면 Out of range 에러 발생
-    # budget > 0을 하면 한 번 더 카운트 되므로 budget >= d[i]로 검사
-    while i < len(d) and budget >= d[i]:
-        budget = budget - d[i]
+    while i < len(d) and budget - d[i] >= 0:
+        budget -= d[i]
         i += 1
-        answer += 1
-
-    return answer
+    return i
