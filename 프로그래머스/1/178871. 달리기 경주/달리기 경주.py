9528d200e0f8165b1c6eff44fc11c2545bd39ece
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-06-03 18:18:59 +0900

    [level 1] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -1,13 +1,11 @@
 def solution(players, callings):
+    rank_hash = {p:i for i, p in enumerate(players)}
     
-    calling_dict = {p:idx for idx, p in enumerate(players)}
-
     for p in callings:
-        idx = calling_dict[p]
-        # idx = players.index(p)    -> 인덱스 찾는데 n, for 문 n => n^2
-
-        players[idx-1], players[idx] = players[idx], players[idx-1]
+        idx = rank_hash[p]
         
-        calling_dict[players[idx]] += 1
-        calling_dict[players[idx-1]] -= 1
+        players[idx], players[idx-1] = players[idx-1], players[idx]
+        rank_hash[players[idx]] += 1
+        rank_hash[players[idx-1]] -= 1
+    
     return players
