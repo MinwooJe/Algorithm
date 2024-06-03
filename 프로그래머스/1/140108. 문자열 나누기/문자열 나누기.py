dea053246fecfcaa88b7d998e769c4fa5c4ad1be
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-06-03 17:56:06 +0900

    [level 1] Title: 문자열 나누기, Time: 1.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -1,15 +1,13 @@
 def solution(s):
-    answer = 0
-    while len(s) > 0:
-        same, not_same = 0, 0
-        x = s[0]
-        i = 0
-        while i < len(s) and (same != not_same or same == 0):
-            if s[i] == x:
-                same += 1
-            else:
-                not_same += 1
-            i += 1
-        answer += 1
-        s = s[i:]
-    return answer
+    x = s[0]
+    count = 0
+    same, not_same = 0, 0
+    for i in s:
+        if same == not_same:
+            count += 1
+            x = i
+        if i == x:
+            same += 1
+        else:
+            not_same += 1
+    return count
