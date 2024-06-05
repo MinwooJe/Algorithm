4c10acde0d3c21c37cb12895337dfcd2e270dce5
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-06-05 14:37:19 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 37.45 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,10 +1,21 @@
 def solution(participant, completion):
-    answer = ''
-    participant.sort()
-    completion.sort()
+    com_hash = {}
+    par_hash = {}
     
-    for a, b in zip(participant, completion):
-        if a != b:
-            return a
-        
-    return participant[-1]
+    for i in completion:
+        if i in com_hash:
+            com_hash[i] += 1
+        else:
+            com_hash[i] = 1
+    
+    for i in participant:
+        if i in par_hash:
+            par_hash[i] += 1
+        else:
+            par_hash[i] = 1
+    # print(par_hash, com_hash)
+    for i in par_hash.keys():
+        if i not in com_hash:
+            return i
+        if par_hash[i] != com_hash[i]:
+            return i
