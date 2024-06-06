4676d7dc68be84b1de0b714c642985d10120a56a
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-06-06 20:04:44 +0900

    [level 1] Title: 대충 만든 자판, Time: 2.10 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -1,23 +1,20 @@
 def solution(keymap, targets):
     key_hash = {}
     answer = []
-    # 딕셔너리 초기화
     for key in keymap:
-        for idx, c in enumerate(key):
-            if c in key_hash:
-                key_hash[c] = min(idx+1, key_hash[c])
+        for i, v in enumerate(key):
+            if v in key_hash:
+                key_hash[v] = min(i+1, key_hash[v])
             else:
-                key_hash[c] = idx + 1
+                key_hash[v] = i+1
     
-    # targets 만들기
     for target in targets:
         count = 0
-        for c in target:
-            if c not in key_hash:
+        for t in target:
+            if t in key_hash:
+                count += key_hash[t]
+            else:
                 count = -1
                 break
-            else:
-                count += key_hash[c]
         answer.append(count)
-        
     return answer
