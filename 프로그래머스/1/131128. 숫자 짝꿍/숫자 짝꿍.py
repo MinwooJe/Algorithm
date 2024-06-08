87a3af0b7049d2a296e2cd015f9ba8eaeb454a2c
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-06-08 23:52:24 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -3,7 +3,7 @@ def solution(X, Y):
     
     for i in range(9, -1, -1):
         answer += str(i) * min(X.count(str(i)), Y.count(str(i)))
-        
+         
     if not answer:
         return '-1'
     elif len(answer) == answer.count('0'):
