fb709e6246f4559d1548e671788716cb60e70666
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-06-11 18:34:20 +0900

    [level 1] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -1,11 +1,14 @@
 def solution(players, callings):
-    rank_hash = {p:i for i, p in enumerate(players)}
+    grade_hash = {p:idx for idx, p in enumerate(players)}
     
     for p in callings:
-        idx = rank_hash[p]
-        
-        players[idx], players[idx-1] = players[idx-1], players[idx]
-        rank_hash[players[idx]] += 1
-        rank_hash[players[idx-1]] -= 1
+        # 추월한사람의 players에서의 인덱스 저장 - grade_hash 이용 -> O(1)
+        idx = grade_hash[p]
+        # players 스왑
+        players[idx-1], players[idx] = players[idx], players[idx-1]
+        # grade_hash 순위 갱신
+        grade_hash[p] -= 1
+        grade_hash[players[idx]] += 1
     
     return players
+        
