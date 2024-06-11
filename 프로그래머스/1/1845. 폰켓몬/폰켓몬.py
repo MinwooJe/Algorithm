4c85545c085a7dc44c73f6420db49012a125fa59
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-06-11 18:39:33 +0900

    [level 1] Title: 폰켓몬, Time: 0.20 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,6 +1,3 @@
 def solution(nums):
-    distinct_len = len(set(nums))
-    if len(nums)/2 > distinct_len:
-        return distinct_len
-    else:
-        return len(nums)/2
+    diff = len(set(nums))
+    return min(len(nums)/2, diff)
