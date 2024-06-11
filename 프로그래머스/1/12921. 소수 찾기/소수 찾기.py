6a88f7b8ad406c59b06ff34783a5586566194cba
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-06-11 18:46:48 +0900

    [level 1] Title: 소수 찾기, Time: 780.96 ms, Memory: 46.9 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,12 +1,9 @@
 def solution(n):
+    end = int(n**0.5)
     nums = [i for i in range(n+1)]
-    end = int(n**0.5) + 1
     
-    for i in range(2, end):
-        if nums[i] == 0:
-            continue
-        for j in range(i+i, n+1, i):
-            nums[j] = 0
-    answer = len([i for i in nums[2:] if nums[i]])
+    for i in range(2, n+1):
+        for i in range(2*i, n+1, i):
+            nums[i] = 0
     
-    return answer
+    return len([i for i in nums if i != 0][1:])
