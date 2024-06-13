dcf35d132acbbef3905e0c0baddb21bb101f9c51
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-06-13 11:44:30 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.64 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,40 +1,35 @@
 def solution(new_id):
-    answer = ''
-    temp = ''
-    # 1st)
-    answer = new_id.lower()
+    # 1단계
+    new_id = new_id.lower()
+    # 2단계
+    removed_special = ''
+    for i in new_id:
+        if i.isdigit() or i.isalpha() or i in ['-', '_', '.']:
+            removed_special += i
+    new_id = removed_special
+    # 3단계
+    stack = []
+    for i in new_id:
+        stack.append(i)
+        if len(stack) > 1 and stack[-1] == '.' and stack[-2] == '.':
+            stack.pop()
+    new_id = ''.join(stack)
+    # 4단계
+    if len(new_id) > 0 and new_id[0] == '.':
+        new_id = new_id[1:]
+    if len(new_id) > 0 and new_id[-1] == '.':
+        new_id = new_id[:-1]
+    # 5단계
+    if not new_id:
+        new_id += 'a'
+    # 6단계
+    if len(new_id) >= 16:
+        new_id = new_id[:15]
+        if new_id[-1] == '.':
+            new_id = new_id[:14]
+    # 7단계
+    padded_char = new_id[-1]
+    while len(new_id) < 3:
+        new_id += padded_char
     
-    # 2nd)
-    for i in answer:
-        if i.isalpha() or i.isdigit() or i in ['-', '_', '.']:
-            temp += i
-    answer = temp
-    
-    # 3rd)
-    while '..' in answer:
-        answer = answer.replace('..', '.')
-
-    # 4th)
-    answer = list(answer)
-    if answer[0] == '.':
-        answer.pop(0)
-    if answer[-1:] == ['.']:
-        answer.pop(-1)
-        
-    # 5th)
-    if not answer:
-        answer += 'a'
-    
-    # 6th)
-    if len(answer) >= 16:
-        answer = answer[:15]
-    if answer[-1] == '.':
-        answer = answer[:-1]
-    
-    # 7th)
-    if len(answer) > 0 or len(answer) <= 2:
-        padded_char = answer[-1]
-        while len(answer) < 3:
-            answer += padded_char
-    answer = ''.join(answer)
-    return answer
+    return new_id
