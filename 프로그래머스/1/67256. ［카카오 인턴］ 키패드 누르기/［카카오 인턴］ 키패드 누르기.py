0cf6aa04d283c52748d6e748f11ea41039d75cc5
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-06-13 11:06:18 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 2.92 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,10 +1,12 @@
 def solution(numbers, hand):
     answer = ''
-    if hand == 'right':
-        hand = 'R'
-    else:
-        hand = 'L'
-    loc_l, loc_r = [3, 0], [3, 2]
+    l_coordinate, r_coordinate = [3, 0], [3, 2]
+    
+    global keypad 
+    keypad = [[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9], 
+              ['*', 0, '#']]
     
     for i in numbers:
         if i in [1, 4, 7]:
@@ -12,34 +14,33 @@ def solution(numbers, hand):
         elif i in [3, 6, 9]:
             answer += 'R'
         else:
-            loc_i = find_location(i)
-            diff_l = get_distance(loc_i, loc_l)
-            diff_r = get_distance(loc_i, loc_r)
-            if diff_l == diff_r:
-                answer += hand
-            elif diff_l < diff_r:
-                answer += 'L'
-            else:
+            l_distance, r_distance = get_distance(i, l_coordinate), get_distance(i, r_coordinate)
+            if l_distance == r_distance:
+                if hand == 'left':
+                    answer += 'L'
+                else:
+                    answer += 'R'
+            elif l_distance > r_distance:
                 answer += 'R'
+            else:
+                answer += 'L'
+                
         if answer[-1] == 'L':
-            loc_l = find_location(i)
+            l_coordinate = get_coordinate(i)
         else:
-            loc_r = find_location(i)
-
+            r_coordinate = get_coordinate(i)
+    
     return answer
+                
+def get_coordinate(n):
+    coord = []
+    for n_col, key_col in enumerate(keypad):
+        if n in key_col:
+            coord.append(n_col)
+            coord.append(key_col.index(n))
+    return coord
 
-def find_location(n):
-    keypad = [[1, 2, 3],
-          [4, 5, 6],
-          [7, 8, 9],
-          ['*', 0, '#']]
-    for keys in keypad:
-        if n in keys:
-            row = keypad.index(keys)
-            col = keys.index(n)
-    return [row, col]
-
-def get_distance(a, b):
-    diff_x = abs(a[1] - b[1])
-    diff_y = abs(a[0] - b[0])
-    return diff_x + diff_y
+def get_distance(n, coordinate):
+    n_coord = get_coordinate(n)
+    distance = abs(n_coord[0] - coordinate[0]) + abs(n_coord[1] - coordinate[1])
+    return distance
