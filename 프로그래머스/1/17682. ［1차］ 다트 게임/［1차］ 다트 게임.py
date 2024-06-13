18710c248641ea31e4430d14d506191b917b9fb1
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-06-13 10:06:54 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,21 +1,23 @@
 def solution(dartResult):
     dartResult = dartResult.replace('10', 'X')
-    answer = []
+    stack = []
+    
     for i in dartResult:
         if i.isdigit():
-            answer.append(int(i))
+            stack.append(int(i))
         elif i == 'X':
-            answer.append(10)
+            stack.append(10)
         elif i == 'D':
-            answer[-1] **= 2
+            stack[-1] **= 2
         elif i == 'T':
-            answer[-1] **= 3
+            stack[-1] **= 3
         elif i == '*':
-            if len(answer) > 1:
-                answer[-1] *= 2
-                answer[-2] *= 2
-            else:
-                answer[-1] *= 2
+            stack[-1] *= 2
+            if len(stack) > 1:
+                stack[-2] *= 2
         elif i == '#':
-            answer[-1] *= -1
-    return sum(answer)
+            stack[-1] *= -1
+
+    answer = sum(stack)
+    
+    return answer
