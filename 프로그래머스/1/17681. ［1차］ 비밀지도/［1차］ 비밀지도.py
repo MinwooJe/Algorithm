6c5fa6ccbe90b567d6b23c85902fe0ba787b2034
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-06-13 09:23:26 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,13 +1,7 @@
 def solution(n, arr1, arr2):
     answer = []
-    # arr의 각 원소들 꺼내서 2진수 or 연산 -> 새로운 배열 만들기
-    for i in range(n):
-        answer.append(bin(arr1[i]|arr2[i])[2:])
-    # 그 배열의 1을 replace #
-    for i in range(n):
-        while len(answer[i]) < n:
-            answer[i] = ' ' + answer[i]
-        answer[i] = answer[i].replace('1', '#')
-        answer[i] = answer[i].replace('0', ' ')
-        
+    for a1, a2 in zip(arr1, arr2):
+        overlapped = bin(a1|a2)[2:].zfill(n)
+        encrypt = overlapped.replace('1', '#').replace('0', ' ')
+        answer.append(encrypt)
     return answer
