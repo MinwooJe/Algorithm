96836fb041726245067038ffe935c6876002222d
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-06-13 09:55:53 +0900

    [level 1] Title: 덧칠하기, Time: 2.67 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,12 +1,11 @@
 def solution(n, m, section):
-    count = 1
+    answer = 1
     start = section[0]
-    
-    for i in range(len(section)-1):
-        if start + m > section[i+1]:
+    for i in section:
+        if i < start + m:
             continue
         else:
-            start = section[i+1]
-            count += 1
-    
-    return count
+            start = i
+            answer += 1
+            
+    return answer
