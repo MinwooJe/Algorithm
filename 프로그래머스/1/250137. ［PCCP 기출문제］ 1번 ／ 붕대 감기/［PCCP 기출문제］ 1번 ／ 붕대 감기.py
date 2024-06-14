a043f1d4975c211e263b7db89f289ee53401086a
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-06-14 19:21:13 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 1.13 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -1,34 +1,27 @@
 def solution(bandage, health, attacks):
-    answer = 0
     hp = health
-    end_time = attacks[-1][0]
-    
-    for i in range(len(attacks)):
+    for idx, atk in enumerate(attacks):        
         # 공격 당함
-        hp -= attacks[i][1]
-        print(f'공격받은 직후 : {hp}')
-        
-        # 사망 시 끝
+        dmg = atk[1]
+        hp -= dmg
+        print('공격 직후: ', hp)
         if hp <= 0:
             return -1
         
-        # 다음 공격까지 남은 시간 구하기
-        if i < len(attacks)-1:
-            cooltime = attacks[i+1][0] - attacks[i][0] - 1
-        else:
-            break
-
-        # 회복
-        count = 0
-        for _ in range(cooltime):
-            count += 1
-            hp += bandage[1]
-            if count % bandage[0] == 0:
-                print('추가 획득')
-                hp += bandage[2]
-            if hp > health:
-                hp = health
-            print('회복 후 : ', hp)
-        count = 0
-    answer = hp
-    return answer
+        # 다음 공격 전까지의 시간 구하기
+        if idx + 1 < len(attacks): # 마지막 인덱스 직전까지
+            cooltime = attacks[idx+1][0] - attacks[idx][0]
+            # 회복
+            count = 0
+            for i in range(cooltime-1):
+                count += 1
+                hp += bandage[1]
+                if count == bandage[0]:
+                    hp += bandage[2]
+                    count = 0
+                if hp > health:
+                    hp = health
+                print(count, '회복 완료 후: ', hp)
+    
+    return hp
+            
