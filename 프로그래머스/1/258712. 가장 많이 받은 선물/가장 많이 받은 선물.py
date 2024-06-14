3f75198721c6186c10837968fd39547c300327f2
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-06-14 20:13:55 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 1.65 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,36 +1,41 @@
 def solution(friends, gifts):
-    n_friends = len(friends)
-    friends_dict =  {v:idx for idx, v in enumerate(friends)}
-    table = [[0 for _ in range(n_friends)] for _ in range(n_friends)]
-    next_month_count = [0 for _ in range(n_friends)]
-    gift_index  = [0 for _ in range(n_friends)]
+    length = len(friends)
+    table = [[0 for _ in range(len(friends))] for _ in range(length)]
+    name_dict = {name:idx for idx, name in enumerate(friends)}
+
+    # index_dict = {이름: [준 선물 개수, 받은 선물 개수]} => 선물지수 계산용
+    index_dict = {idx:[0, 0] for idx in range(length)}
+    gift_index = [0 for _ in range(length)]
     
-    # 주고 받은 개수 테이블 만들기
-    for idx, gift in enumerate(gifts):
+    next_month = [0 for _ in range(length)]
+    
+    # 주고받은 선물의 개수를 표현하는 테이블 만들기
+    for gift in gifts:
         giver, taker = gift.split()
-        table[friends_dict[giver]][friends_dict[taker]] += 1
-        gifts[idx] = [friends_dict[giver], friends_dict[taker]]
+        giver, taker = name_dict[giver], name_dict[taker]
+
+        table[giver][taker] += 1        # 테이블 게산
     
-    # 선물 지수 계산
-    for i in range(n_friends):
-        gift_index[i] += sum(table[i])
-        for j in range(n_friends):
-            gift_index[i] -= table[j][i]
-    print(f'선물 지수 : {gift_index}')
-    for i in range(n_friends):
-        for j in range(i, n_friends):
+        # 선물지수 계산하기 위한 딕셔너리 만들기
+        index_dict[giver][0] += 1
+        index_dict[taker][1] += 1
+        
+    # 선물지수 리스트 계산
+    for i in range(length):
+        gift_index[i] = index_dict[i][0] - index_dict[i][1]
+
+    # 다음 달 받을 선물 개수 계산
+    for i in range(length):
+        for j in range(i, length):
             if i != j:
-                if table[i][j] > table[j][i]:	# i->j > j->i 일때
-                    next_month_count[i] += 1
-                elif table[i][j] < table[j][i]:	# j->i > i->j 일때
-                    next_month_count[j] += 1
+                if table[i][j] > table[j][i]:
+                    next_month[i] += 1
+                elif table[i][j] < table[j][i]:
+                    next_month[j] += 1
                 else:
-                    # 선물 지수 비교
                     if gift_index[i] > gift_index[j]:
-                    	next_month_count[i] += 1
+                        next_month[i] += 1
                     elif gift_index[i] < gift_index[j]:
-                    	next_month_count[j] += 1
-	
-    answer = max(next_month_count)
-    
-    return answer
+                        next_month[j] += 1
+
+    return max(next_month)
