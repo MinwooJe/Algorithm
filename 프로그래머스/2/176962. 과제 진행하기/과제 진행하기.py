b4a83c2f16789ef784ef9d488df07884bfcb958a
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-06-14 22:01:34 +0900

    [level 2] Title: 과제 진행하기, Time: 1.42 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -1,27 +1,27 @@
 def solution(plans):
     plans = sorted(plans, key=lambda x: x[1])
-    stack = []
     answer = []
+    stack = []
     
-    # 시간 분으로 바꾸기 ex) 03:30 -> 210
+    # 시간 변환
     for i in range(len(plans)):
-        h, m = map(int, plans[i][1].split(':'))
-        plans[i][1] = h*60 + m
+        h, m = plans[i][1].split(':')
+        plans[i][1] = int(h) * 60 + int(m)
         plans[i][2] = int(plans[i][2])
-
+    
     for idx, plan in enumerate(plans):
         stack.append(plan)
-        # 다음 과제와 시간 차 구하기 - 스택에 들어올 때
-        if idx+1 < len(plans):
+        # 다음 과제까지 남은 시간 구하기
+        if idx + 1 < len(plans):
             delta = plans[idx+1][1] - plans[idx][1]
-            # 소요시간을 시간차만큼 빼기  - 시간차가 한과목 끝내고도 남는 경우 처리
-            while stack and stack[-1][2] <= delta:
+        
+            # 소요시간 - delta -> 만약 delta가 엄청 커서 여러 과제들 수ㅐㅎㅇ 가능하다면?
+            while stack and delta >= stack[-1][2]:
                 delta -= stack[-1][2]
                 answer.append(stack.pop()[0])
             if stack:
                 stack[-1][2] -= delta
-                
-    # 모든 과제가 다 들어왔을 때
+
     while stack:
         answer.append(stack.pop()[0])
     
