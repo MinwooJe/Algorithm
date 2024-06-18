553d427a955cc2a31286eeab58c6bdec9f7f617d
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-06-18 21:07:41 +0900

    [level 2] Title: 짝지어 제거하기, Time: 189.22 ms, Memory: 14.7 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,14 +1,14 @@
 def solution(s):
     stack = []
     
-    for i in s:
-        stack.append(i)
+    for c in s:
+        stack.append(c)
         if len(stack) > 1 and stack[-1] == stack[-2]:
             stack.pop()
             stack.pop()
+    
     if stack:
         return 0
     else:
         return 1
-
-    return answer
+    
