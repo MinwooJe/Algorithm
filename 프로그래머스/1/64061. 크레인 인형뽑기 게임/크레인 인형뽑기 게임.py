7b08226f1ea8fe191d655cb617ec1710f913d8e6
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-06-18 15:43:20 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.15 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,25 +1,19 @@
 def solution(board, moves):
     basket = []
     count = 0
-    # move의 원소-1은 board의 열을 인덱스 의미.
-    # 해당 열로 가서 while 원소 == 0: 행++
-    for col_idx in moves:
-        col_idx -= 1
-        row_idx = 0
-        while row_idx < len(board)-1 and board[row_idx][col_idx] == 0:
-            row_idx += 1
+    
+    for col in moves:
+        col -= 1
+        row = 0
+        while row < len(board)-1 and board[row][col] == 0:
+            row += 1
         
-        if board[row_idx][col_idx] != 0:
-            basket.append(board[row_idx][col_idx])
-            board[row_idx][col_idx] = 0
-        
-        if len(basket) > 1:
-            if basket[-1] == basket[-2]:
+        if board[row][col] != 0:
+            basket.append(board[row][col])
+            board[row][col] = 0
+            if len(basket) > 1 and basket[-1] == basket[-2]:
                 basket.pop()
                 basket.pop()
                 count += 2
-        
+    
     return count
-    # 만약 해당 열이 모두 0이라면 인덱스가 len(board)를 넘음. -> 예외처리 해주고
-    # 아니면 해당 원소 스택에 넣고 board에서 0으로 바꾸기
-    # 스택의 맨 마지막 두 원소가 같다면 pop 두 번
