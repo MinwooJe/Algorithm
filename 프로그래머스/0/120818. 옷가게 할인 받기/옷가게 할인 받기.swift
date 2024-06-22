217329cb8ed4702fdf7f341e5a25d939b6ab1369
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-06-22 20:04:41 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.04 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.swift
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.swift
@@ -2,15 +2,16 @@ import Foundation
 
 func solution(_ price:Int) -> Int {
     var answer = Double(price)
-
-    if price >= 500000 {
-        answer = Double(price) * 0.8
-    } else if price >= 300000 {
-        answer = Double(price) * 0.9
-    } else if price >= 100000 {
-        answer = Double(price) * 0.95
-    } else {
-        
+    
+    switch price {
+        case 500000...: 
+            answer = Double(price) * 0.8
+        case 300000...500000:
+            answer = Double(price) * 0.9
+        case 100000...300000:
+            answer = Double(price) * 0.95
+        default:
+            break
     }
     
     return Int(answer)
