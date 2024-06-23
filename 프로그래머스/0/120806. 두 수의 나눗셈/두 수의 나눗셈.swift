052403b0b6126eee4601fb4b2dc25ca848ed5cd7
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-06-24 04:09:33 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.swift
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.swift
@@ -1,7 +1,6 @@
 import Foundation
 
 func solution(_ num1:Int, _ num2:Int) -> Int {
-    var result = Int(Double(num1) / Double(num2) * 1000)
-    
-    return result
+    var answer = Int(Double(num1) / Double(num2) * 1000)
+    return answer
 }
