e210a8fe3188b23ff43f552b6b48eac706687332
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-07-02 14:29:54 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 0.02 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.swift
+++ 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.swift
@@ -1,10 +1,5 @@
 import Foundation
 
 func solution(_ my_string:String, _ n:Int) -> String {
-    let startIdx = my_string.startIndex
-    let endIdx = my_string.index(startIdx, offsetBy: n)
-
-    let answer = String(my_string[startIdx..<endIdx])
-
-    return answer
+    return String(my_string.prefix(n))
 }
