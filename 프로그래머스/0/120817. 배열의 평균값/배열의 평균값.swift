8cc0234f3eb481f2eb054d564986f57206bfb768
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-07-02 11:02:21 +0900

    [level 0] Title: 배열의 평균값, Time: 0.03 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.swift
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 func solution(_ numbers:[Int]) -> Double {
-    let answer = Double(numbers.reduce(0, +)) / Double(numbers.count)
-    return answer
+    let result = Double(numbers.reduce(0, +)) / Double(numbers.count)
+    return result
 }
