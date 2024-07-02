dc11145abf340d9226be10fdf51f43238425aea8
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-07-02 11:06:34 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.swift
+++ 프로그래머스/0/120829. 각도기/각도기.swift
@@ -1,5 +1,16 @@
 import Foundation
 
 func solution(_ angle:Int) -> Int {
-    return angle < 90 ? 1 : angle == 90 ? 2 : angle < 180 ? 3 : 4
+    switch angle {
+        case 0..<90:
+            return 1
+        case 90:
+            return 2
+        case 90..<180:
+            return 3
+        case 180:
+            return 4
+        default:
+            return 0
+    }
 }
