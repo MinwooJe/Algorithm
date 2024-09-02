b5ec89e54de1bb8ab298ecb59dfd7e0bce645584
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-09-02 15:42:59 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.11 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.swift
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.swift
@@ -1,16 +1,8 @@
 func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
-    var answer: [String] = []
-    
-    for (a1, a2) in zip(arr1, arr2) {
-        let overlapped = String(a1 | a2, radix: 2)   
-        let formmated = String(repeating: "0", count: n - overlapped.count) + overlapped
-
-        let crypto = formmated
-        .replacingOccurrences(of: "0", with: " ")
-        .replacingOccurrences(of: "1", with: "#")
-        
-        answer.append(crypto)
+    return (0..<n).map {
+        let overlapped = String(arr1[$0] | arr2[$0], radix: 2)
+        let padded = String(repeating: "0", count: n - overlapped.count) + overlapped
+        return padded.reduce("") { $0 + ($1 == "0" ? " " : "#")}
     }
-
-    return answer
 }
+
