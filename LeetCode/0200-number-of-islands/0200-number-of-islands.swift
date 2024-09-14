b6fd2d27d8cee4edee15ea55e606485a472a305d
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-09-14 23:00:10 +0900

    Time: 154 ms (63.80%), Space: 18.1 MB (91.10%) - LeetHub

--- LeetCode/0200-number-of-islands/0200-number-of-islands.swift
+++ LeetCode/0200-number-of-islands/0200-number-of-islands.swift
@@ -2,42 +2,49 @@ class Solution {
     func numIslands(_ grid: [[Character]]) -> Int {
         let rowCount = grid.count
         let colCount = grid[0].count
-        
         var result = 0
         var visited = Array(repeating: Array(repeating: false, count: colCount), count: rowCount)
         
+        // grid 하나의 원소 씩 순회
         for row in 0..<rowCount {
             for col in 0..<colCount {
-                if grid[row][col] == "1" && visited[row][col] == false {
+                if grid[row][col] == "1" && !visited[row][col] {
                     result += 1
                     bfs(row, col)
                 }
             }
         }
-        
+
         func bfs(_ row: Int, _ col: Int) {
             var queue = [(Int, Int)]()
+            let dRow = [1, -1, 0, 0]
+            let dCol = [0, 0, -1, 1]
+
             queue.append((row, col))
-            
-            let dr = [-1, 1, 0, 0]
-            let dc = [0, 0, -1, 1]
+            visited[row][col] = false   // 얘가 35라인에서도 중복됨
             while !queue.isEmpty {
+                // 큐에서 꺼내기
                 let (curRow, curCol) = queue.removeFirst()
-                
+                // 상하좌우 순회
                 for i in 0..<4 {
-                    let nextRow = curRow + dr[i]
-                    let nextCol = curCol + dc[i]
+                    let nextRow = curRow + dRow[i]
+                    let nextCol = curCol + dCol[i]
+                    
                     if nextRow >= 0 && nextRow < rowCount && nextCol >= 0 && nextCol < colCount {
-                        if !visited[nextRow][nextCol] && grid[nextRow][nextCol] == "1" {
-                            visited[nextRow][nextCol] = true
+                        if grid[nextRow][nextCol] == "1" && !visited[nextRow][nextCol] {
                             queue.append((nextRow, nextCol))
+                            visited[nextRow][nextCol] = true
                         }
                     }
                 }
-                
             }
         }
-        
         return result
     }
 }
+
+let s = Solution()
+print(s.numIslands(  [["1","1","0","0","0"],
+               ["1","1","0","0","0"],
+               ["0","0","1","0","0"],
+               ["0","0","0","1","1"]]))
