77209f71c01c879bec8326f4957c956404391b0c
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-09-18 21:36:41 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+func solution(_ n:Int) -> Int 
+{
+    var ans:Int = 0
+    var n = n
+    
+    while n > 0 {
+        if n % 2 == 0 {
+            n /= 2
+        } else {
+            n -= 1
+            ans += 1
+        }
+    }
+    
+    return ans
+}
