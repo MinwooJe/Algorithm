38e9db2dfa4f278e14bbaae8f94c3b2b122213b7
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-09-19 12:42:17 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.22 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.swift
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.swift
@@ -1,40 +1,34 @@
+import Foundation
+
 func solution(_ dartResult:String) -> Int {
     var points = [Int]()
     let dartResult = dartResult.replacingOccurrences(of: "10", with: "x")
     for c in dartResult {
-        if c.isNumber || c == "x" {
-            if c == "x" {
-                points.append(10)
-                continue
-            }
-            points.append(Int(String(c))!)
-            continue
-        }
-        
         let lastIndex = points.count - 1
-        if c == "S" {
-            continue
-        }
-        if c == "D" {
-            points[lastIndex] = points[lastIndex] * points[lastIndex]
-            continue
-        }
-        if c == "T" {
-            points[lastIndex] = points[lastIndex] * points[lastIndex] * points[lastIndex]
-            continue
-        }
         
-        if c == "*" {
-            points[lastIndex] = points[lastIndex] * 2
-            if lastIndex - 1 >= 0 {
-                points[lastIndex - 1] = points[lastIndex - 1] * 2
+        if c.isNumber {
+            points.append(Int(String(c))!)
+        } else if c == "x" {
+            points.append(10)
+        } else {
+            switch c {
+            case "S":
+                continue
+            case "D":
+                points[lastIndex] = points[lastIndex] * points[lastIndex]
+            case "T":
+                points[lastIndex] = points[lastIndex] * points[lastIndex] * points[lastIndex]
+            case "*":
+                points[lastIndex] = points[lastIndex] * 2
+                if lastIndex - 1 >= 0 {
+                    points[lastIndex - 1] = points[lastIndex - 1] * 2
+                }
+            case "#":
+                points[lastIndex] = points[lastIndex] * -1
+            default:
+                break
             }
         }
-        if c == "#" {
-            points[lastIndex] = points[lastIndex] * -1      
-        }
-        
     }
-    print(points)
     return points.reduce(0, +)
 }
