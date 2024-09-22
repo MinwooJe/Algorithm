c63edd6fa0e132c28b8afca8ac274c7059c58555
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-09-22 12:48:38 +0900

    [level 1] Title: 추억 점수, Time: 2.70 ms, Memory: 17.2 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.swift
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.swift
@@ -1,6 +1,15 @@
 import Foundation
 
-func solution(_ name:[String], _ yearning:[Int], _ photo:[[String]]) -> [Int] {
-    let yearningDict = Dictionary(uniqueKeysWithValues: zip(name, yearning))
-    return photo.map { $0.reduce(0) { $0 + (yearningDict[$1] ?? 0) } }
+func solution(_ name:[String], _ yearning:[Int], _ photos:[[String]]) -> [Int] {
+    var result = [Int]()
+    var yearningDict = Dictionary(uniqueKeysWithValues: zip(name, yearning))
+
+    for photo in photos {
+        var score = 0
+        for people in photo {
+            score += yearningDict[people, default: 0]
+        }
+        result.append(score)
+    }
+    return result
 }
