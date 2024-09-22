cc1cc18e57e26c1d9aa9d67af748b401c56ee8a9
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-09-22 11:23:13 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.09 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.swift
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.swift
@@ -1,20 +1,20 @@
-func solution(_ s:String) -> String {    
-    var wordIndex = 0
-    var result = ""
+func solution(_ s:String) -> String {
+    // 문자열 쫙 돌기
+    // 공백 나오면 인덱스 초기화
+    var s = Array(s.lowercased()).map { String($0) }
+    var wordIndex: Int = 0
     
-    for (index, value) in s.enumerated() {
-        if value.isWhitespace {
+    for i in 0..<s.count {
+        if s[i] == " " {
             wordIndex = 0
-            result += String(value)
         } else {
             if wordIndex % 2 == 0 {
-                result += value.uppercased()
-            } else {
-                result += value.lowercased()
+                s[i] = s[i].uppercased()
             }
+            
             wordIndex += 1
         }
     }
     
-    return result
+    return s.joined(separator: "")
 }
