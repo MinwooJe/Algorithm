69bf04fa019b32cc5c69d54a99f1c02d2edd3a90
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-09-22 12:38:21 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 574.23 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.swift
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.swift
@@ -1,15 +1,15 @@
 import Foundation
 
 func solution(_ t:String, _ p:String) -> Int {
+    var result = 0
     let iterCount = t.count - p.count + 1
-    var count = 0
     
     for i in 0..<iterCount {
-        let start = t.index(t.startIndex, offsetBy: i)
-        let end = t.index(start, offsetBy: p.count - 1)
-        if t[start...end] <= p {
-            count += 1
+        let startIndex = t.index(t.startIndex, offsetBy: i)
+        let endIndex = t.index(startIndex, offsetBy: p.count - 1)
+        if t[startIndex...endIndex] <= p {
+            result += 1
         }
     }
-    return count
+    return result
 }
