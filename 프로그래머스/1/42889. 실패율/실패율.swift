1cf2805f333f2b5140495b91734e0f93511117bf
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-09-22 14:51:30 +0900

    [level 1] Title: 실패율, Time: 113.90 ms, Memory: 25.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.swift
+++ 프로그래머스/1/42889. 실패율/실패율.swift
@@ -1,19 +1,19 @@
 import Foundation
 
 func solution(_ N:Int, _ stages:[Int]) -> [Int] {
-    var challenger = Array(repeating: 0, count: N + 1)
+    var challengers = Dictionary(uniqueKeysWithValues: (1...N+1).map { ($0, 0) })
     var failRateDict = [Int: Double]()
+    
+    stages.forEach { challengers[$0]! += 1 }
+    
     var total = stages.count
-    
-    stages.forEach { challenger[$0 - 1] += 1 }
-    
     for i in 1...N {
-        failRateDict[i] = Double(challenger[i - 1]) / Double(total)
-        total -= challenger[i - 1]
+        failRateDict[i] = Double(challengers[i]!) / Double(total)
+        total -= challengers[i]!
     }
-
+    
     return failRateDict
-    .sorted { $0.key < $1.key }
-    .sorted { $0.value > $1.value}
-    .map { $0.key }
+        .sorted { $0.key < $1.key }
+        .sorted { $0.value > $1.value }
+        .map { $0.key }
 }
