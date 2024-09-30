06dd2bdb64fdbf975fefe582c2077fbaec172185
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-09-30 10:40:10 +0900

    Time: 14 ms (24.93%), Space: 15.9 MB (75.98%) - LeetHub

--- LeetCode/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.swift
+++ LeetCode/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.swift
@@ -14,12 +14,12 @@
  * }
  */
 class Solution {
-    func maxDepth(_ root: TreeNode?) -> Int {        
+    func maxDepth(_ root: TreeNode?) -> Int {
         guard let root else { return 0 }
         
-        let leftDepth = maxDepth(root.left)
-        let rightDepth = maxDepth(root.right)
-        
-        return max(leftDepth, rightDepth) + 1
+        let left = maxDepth(root.left)
+        let right = maxDepth(root.right)
+
+        return max(left, right) + 1
     }
 }
