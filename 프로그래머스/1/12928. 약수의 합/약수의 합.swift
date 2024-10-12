10630d8d90e575c3c2f4b87b03f246ac5a53e452
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-10-12 21:51:23 +0900

    [level 1] Title: 약수의 합, Time: 0.02 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.swift
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.swift
@@ -1,6 +1,20 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-    if n == 0 { return 0 }
-    return Array(1...n).filter { return n % $0 == 0 }.reduce(0, +)
+    guard n != 0 else { return 0 }
+    var result = 0
+    let sqrtN = Int(sqrt(Double(n)))
+    
+    for i in 1...sqrtN {
+        if n % i == 0 {
+            if i * i == n {
+                result += i
+            } else {
+                result += i
+                result += n / i
+            }
+        }
+    }
+    
+    return result
 }
