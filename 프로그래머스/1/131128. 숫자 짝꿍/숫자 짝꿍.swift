e0648df606264b786583ceb1477eb5cfb017c81e
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-10-12 20:59:47 +0900

    [level 1] Title: 숫자 짝꿍, Time: 6468.45 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.swift
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.swift
@@ -1,35 +1,26 @@
 import Foundation
 
 func solution(_ X:String, _ Y:String) -> String {
-    var xHash = [Character: Int]()
-    var yHash = [Character: Int]()
     var pairHash = [Character: Int]()
     var result = ""
     
-    for x in X {
-        xHash[x, default: 0] += 1
-    }
-    for y in Y {
-        yHash[y, default: 0] += 1
-    }
-    
-    for x in xHash.keys {
-        guard let yCount = yHash[x] else { continue }
-        pairHash[x] = min(xHash[x]!, yCount)
+    (0...9).forEach { num in
+        let numCountX = X.filter { $0 == Character(String(num)) }.count
+        let numCountY = Y.filter { $0 == Character(String(num)) }.count
+        
+        pairHash[Character(String(num)), default: 0] = min(numCountX, numCountY)
     }
     
-    // 숫자 문자열을 배열로 대체하여 역순으로 처리
-    for k in (0...9).reversed() {
-        if let count = pairHash[Character(String(k))] {
-            result += String(repeating: String(k), count: count)
-        }
+    for i in (0...9).reversed() {
+        let num = Character(String(i))
+        result += String(repeating: num, count: pairHash[num] ?? 0)
     }
     
     if result.isEmpty {
         return "-1"
     }
     
-    if result.allSatisfy({ $0 == "0" }) {
+    if result.allSatisfy( { $0 == "0"} ) {
         return "0"
     }
     
