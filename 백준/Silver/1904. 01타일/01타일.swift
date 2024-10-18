7fe5ff3a91adf6e1b3ccb2970b339301d898b65e
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-10-18 18:39:36 +0900

    [Silver III] Title: 01타일, Time: 360 ms, Memory: 212292 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.swift
@@ -0,0 +1,19 @@
+let n = Int(readLine()!)!
+var memo = [Int: Int]()
+
+func dp(_ n: Int) -> Int {
+    if n == 1 {
+        return 1
+    }
+    if n == 2 {
+        return 2
+    }
+    
+    if !memo.keys.contains(n) {
+        memo[n] = (dp(n - 1) + dp(n - 2)) % 15746
+    }
+    
+    return memo[n]!
+}
+
+print(dp(n))
