f38d5e90e65b068173ff1ab53863d857fe5ab1b5
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-10-18 13:30:43 +0900

    Time: 0 ms (100.00%), Space: 17.8 MB (7.11%) - LeetHub

--- LeetCode/0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.swift
+++ LeetCode/0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.swift
@@ -1,15 +1,14 @@
 class Solution {
     func minCostClimbingStairs(_ cost: [Int]) -> Int {
         func dp(_ n: Int) -> Int {
-            var memo = [Int: Int]()
-            memo[1] = 0
-            memo[2] = 0
+            var memo = [0: 0, 1: 0]
             
-            for i in 3...n {
-                memo[i] = min(memo[i - 1]! + cost[i - 2], memo[i - 2]! + cost[i - 3])
+            for i in 2...n {
+                memo[i] = min(memo[i - 1]! + cost[i - 1], memo[i - 2]! + cost[i - 2])
             }
             return memo[n]!
         }
-        return dp(cost.count + 1)
+        
+        return dp(cost.count)
     }
 }
