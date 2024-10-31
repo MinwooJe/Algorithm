abd93a7a7d5ac08905835c2cad7db551c0b4ed26
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-10-31 22:51:18 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/README.md
+++ 프로그래머스/1/12930. 이상한 문자 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 이상한 문자 만들기 - 12930 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930?language=python3) 
 
 ### 성능 요약
 
-메모리: 16.5 MB, 시간: 0.09 ms
+메모리: 10.1 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 22일 11:23:10
+2024년 10월 31일 22:51:16
 
 ### 문제 설명
 
--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,15 +1,14 @@
 def solution(s):
-    s = list(s)
-    word_idx = 0
+    arr = list(s)
+    index = 0
     
-    for idx, c in enumerate(s):
-        if c == ' ':
-            word_idx = 0
-        else:   
-            if word_idx % 2 == 0:
-                s[idx] = c.upper()
-            else:
-                s[idx] = c.lower()
-            word_idx += 1
-    
-    return ''.join(s)
+    for i in range(len(arr)):
+        if arr[i] == ' ':
+            index = 0
+            continue
+        if index % 2 == 0:
+            arr[i] = arr[i].upper()
+        else:
+            arr[i] = arr[i].lower()
+        index += 1
+    return ''.join(arr)
