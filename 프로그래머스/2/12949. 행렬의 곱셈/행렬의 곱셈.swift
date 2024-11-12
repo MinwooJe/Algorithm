96ecc526c5de64632b7b7c11b4651b760469fc0b
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-11-12 12:16:17 +0900

    [level 2] Title: 행렬의 곱셈, Time: 223.97 ms, Memory: 17.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
+    var result = Array(
+        repeating: Array(repeating: 0, count: arr2[0].count), 
+        count: arr1.count
+    )
+    
+    for row in 0..<arr1.count {
+        for col in 0..<arr2[0].count {
+            result[row][col] = (0..<arr1[0].count)
+                .map { arr1[row][$0] * arr2[$0][col] }
+                .reduce(0, +)
+        }
+    }
+    return result
+}
