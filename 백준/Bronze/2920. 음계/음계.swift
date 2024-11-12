0b6408453546f4105665e92e4aee4eb8abe5358d
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-11-12 14:43:09 +0900

    [Bronze II] Title: 음계, Time: 8 ms, Memory: 69096 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.swift
@@ -0,0 +1,11 @@
+let input = readLine()!
+let asc = "1 2 3 4 5 6 7 8"
+let des = "8 7 6 5 4 3 2 1"
+
+if input == asc {
+    print("ascending")
+} else if input == des {
+    print("descending")
+} else {
+    print("mixed")
+}
