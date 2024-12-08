9eed24e1f386dd64a1a42e97d659327d637967c4
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-12-08 15:05:51 +0900

    [level 1] Title: 덧칠하기, Time: 7.33 ms, Memory: 19.5 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.swift
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.swift
@@ -1,15 +1,14 @@
 import Foundation
 
 func solution(_ n:Int, _ m:Int, _ section:[Int]) -> Int {
-    var result: Int = 0
-    var lastSection: Int = 0
+    var result = 0
+    var endOfPaintedIdx = 0
     
     for s in section {
-        if s <= lastSection {
-            continue
+        if s > endOfPaintedIdx {
+            endOfPaintedIdx = s + m - 1
+            result += 1
         }
-        lastSection = s + m - 1
-        result += 1
     }
     
     return result
