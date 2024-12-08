6dd1fe91cfb003f1c35a0e2430f8d1fbf1686892
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-12-08 11:31:57 +0900

    [level 1] Title: 행렬의 덧셈, Time: 75.48 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.swift
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.swift
@@ -1,13 +1,3 @@
 func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
-    let rowCount = arr1.count
-    let colCount = arr1[0].count
-    var result = Array(repeating: Array(repeating: 0, count: colCount), count: rowCount)
-    
-    for row in 0..<rowCount {
-        for col in 0..<colCount {
-            result[row][col] = arr1[row][col] + arr2[row][col]
-        }
-    }
-    
-    return result
+    return zip(arr1, arr2).map { zip($0, $1).map { $0 + $1 } }
 }
