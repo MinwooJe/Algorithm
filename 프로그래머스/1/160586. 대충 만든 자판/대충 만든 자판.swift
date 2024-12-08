b8b7a8d5c4c139bb90f4000dcb2bf7041eb76cda
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-12-08 15:23:14 +0900

    [level 1] Title: 대충 만든 자판, Time: 3.61 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.swift
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.swift
@@ -1,29 +1,24 @@
 import Foundation
 
 func solution(_ keymap:[String], _ targets:[String]) -> [Int] {
+    let maximumTyping = 999
+    var minTypingDict = [Character: Int]()
     var result = [Int]()
-    var keyDict = [Character: Int]()
     
     for key in keymap {
-        key.enumerated().forEach { (index, k) in
-            if keyDict[k] == nil {
-                keyDict[k] = index
-            } else {
-                keyDict[k] = min(keyDict[k]!, index)
-            }
+        for (idx, c) in key.enumerated() {
+            minTypingDict[c] = min(idx + 1, minTypingDict[c, default: maximumTyping])
         }
     }
-    
+
     for target in targets {
         var sum = 0
-        
         for t in target {
-            if keyDict[t] == nil {
+            guard let count = minTypingDict[t] else {
                 sum = -1
                 break
-            } else {
-                sum += keyDict[t]! + 1
             }
+            sum += count
         }
         result.append(sum)
     }
