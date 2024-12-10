658d90eb78d897247e118df9784cd154d28d42bc
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-12-10 13:17:36 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.15 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/README.md
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/README.md
@@ -1,10 +1,10 @@
 # [level 1] [1차] 비밀지도 - 17681 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681) 
 
 ### 성능 요약
 
-메모리: 16.6 MB, 시간: 0.11 ms
+메모리: 16.6 MB, 시간: 0.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 02일 15:42:57
+2024년 12월 10일 13:17:35
 
 ### 문제 설명
 
--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.swift
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.swift
@@ -1,8 +1,8 @@
 func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
     return (0..<n).map {
-        let overlapped = String(arr1[$0] | arr2[$0], radix: 2)
-        let padded = String(repeating: "0", count: n - overlapped.count) + overlapped
-        return padded.reduce("") { $0 + ($1 == "0" ? " " : "#")}
+        let entireMap = String(arr1[$0] | arr2[$0], radix: 2)
+        let paddedMap = String(repeating: "0", count: n - entireMap.count) + entireMap
+        return paddedMap.reduce("") { $0 + ($1 == "0" ? " " : "#")}
     }
 }
 
