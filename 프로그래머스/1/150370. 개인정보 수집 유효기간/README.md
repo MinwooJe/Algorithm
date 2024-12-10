99315649dc977f6c68f0576c8edf98a93c009afe
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-12-10 13:38:37 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 1.01 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/README.md
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/README.md
@@ -1,10 +1,10 @@
 # [level 1] 개인정보 수집 유효기간 - 150370 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150370?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150370) 
 
 ### 성능 요약
 
-메모리: 16.4 MB, 시간: 1.05 ms
+메모리: 16.5 MB, 시간: 1.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 16일 15:58:01
+2024년 12월 10일 13:38:35
 
 ### 문제 설명
 
--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.swift
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.swift
@@ -1,37 +1,31 @@
 import Foundation
 
 func solution(_ today:String, _ terms:[String], _ privacies:[String]) -> [Int] {
-    let today = parseDateToDay(today)
+    let today = convertToDays(for: today)
     var termsDict = [String: Int]()
     var result = [Int]()
     
     for term in terms {
-        let components = term
-        .split(separator: " ")
-        .map { String($0) }
-        termsDict[components[0]] = Int(components[1])!
+        let term = term.split(separator: " ").map { String($0) }
+        termsDict[term[0]] = Int(term[1])!
     }
-    
-    for (index, privacy) in privacies.enumerated() {
-        let components = privacy
-        .split(separator: " ")
-        .map { String($0) }
-        let startDate = parseDateToDay(components[0])
-        let dueDayByTerm: Int = termsDict[components[1]]! * 28
+
+    for (idx, privacy) in privacies.enumerated() {
+        let components = privacy.split(separator: " ").map { String($0) }
+        let dueDate = convertToDays(for: components[0]) + termsDict[components[1]]! * 28
         
-        if startDate + dueDayByTerm <= today {
-            result.append(index + 1)
+        if today >= dueDate {
+            result.append(idx + 1)
         }
     }
     return result
 }
 
-// YYYY-MM-DD나 HH-MM-SS는 숫자 하나로 바꾸는게 편함
-// ex. 3시 5분 5초 -> (3*60*60 + 5*60 + 6) 초
-func parseDateToDay(_ date: String) -> Int {
-    let components = date
-    .split(separator: ".")
-    .map { Int($0)! }
+func convertToDays(for date: String) -> Int {
+    let components = date.split(separator: ".")
+    let year = Int(components[0])!
+    let month = Int(components[1])!
+    let day = Int(components[2])!
     
-    return components[0] * 12 * 28 + components[1] * 28 + components[2]
+    return (year * 12 * 28) + (month * 28) + day
 }
