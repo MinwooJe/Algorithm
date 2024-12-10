c16a0535dc39359913f5e85501d508a6bb20c00d
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-12-10 15:54:38 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 7.08 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.swift
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.swift
@@ -1,6 +1,7 @@
 func solution(_ strings:[String], _ n:Int) -> [String] {
     return strings
-        .sorted { $0 < $1 }
-        .sorted { Array($0)[n] < Array($1)[n] }
-        .map { String($0) }
+            .sorted()
+            .sorted {
+                Array($0)[n] < Array($1)[n]
+            }
 }
