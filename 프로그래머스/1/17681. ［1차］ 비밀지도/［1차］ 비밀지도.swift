ec177ae47be8f6bca4338964c213d6a9f761ff9e
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-12-12 22:25:26 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.10 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.swift
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.swift
@@ -1,8 +1,7 @@
 func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
     return (0..<n).map {
-        let entireMap = String(arr1[$0] | arr2[$0], radix: 2)
-        let paddedMap = String(repeating: "0", count: n - entireMap.count) + entireMap
-        return paddedMap.reduce("") { $0 + ($1 == "0" ? " " : "#")}
+        let entireRow = String(arr1[$0] | arr2[$0], radix: 2)
+        let paddedRow = String(repeating: "0", count: n - entireRow.count) + entireRow
+        return paddedRow.reduce("") { $0 + ($1 == "0" ? " " : "#")}
     }
 }
-
