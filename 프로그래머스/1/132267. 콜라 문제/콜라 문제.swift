a7ea0fe0c57337da6ca9ca69d8fa06c8100b882f
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-12-13 13:24:46 +0900

    [level 1] Title: 콜라 문제, Time: 0.05 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.swift
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.swift
@@ -1,13 +1,14 @@
 import Foundation
 
 func solution(_ a:Int, _ b:Int, _ n:Int) -> Int {
-    var currBottle = n
     var result = 0
-    
-    while currBottle >= a {
-        let newBottle = (currBottle / a) * b
-        currBottle += newBottle - (currBottle / a) * a
-        result += newBottle
+    var emptyBottle = n
+
+    while emptyBottle >= a {
+        let emptyBottleBundle = emptyBottle / a
+        // emptyBottle += -emptyBottleBundle * a + emptyBottleBundle * b
+        emptyBottle += (b - a) * emptyBottleBundle
+        result += b * emptyBottleBundle
     }
     
     return result
