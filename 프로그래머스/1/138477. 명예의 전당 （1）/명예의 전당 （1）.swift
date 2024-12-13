250c12da13ecdc6daaafb50772c260a32326fc97
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-12-13 12:21:24 +0900

    [level 1] Title: 명예의 전당 (1), Time: 17.82 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.swift
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.swift
@@ -5,13 +5,11 @@ func solution(_ k:Int, _ score:[Int]) -> [Int] {
     var result = [Int]()
     
     for s in score {
-        if hallOfFame.count == k && s > hallOfFame[k - 1] {
-            hallOfFame[k - 1] = s
-        }
-        if hallOfFame.count < k {
+        if hallOfFame.count == k {
+            hallOfFame[k - 1] = max(hallOfFame[k - 1], s)
+        } else {
             hallOfFame.append(s)
         }
-        
         hallOfFame = hallOfFame.sorted(by: >)
         result.append(hallOfFame[hallOfFame.count - 1])
     }
