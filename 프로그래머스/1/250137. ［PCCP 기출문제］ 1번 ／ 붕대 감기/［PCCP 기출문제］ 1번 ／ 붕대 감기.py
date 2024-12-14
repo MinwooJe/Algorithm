6776ac37e3bc4a6ffe5f9c67e183672ada14ebb3
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-12-14 18:00:02 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -1,27 +1,28 @@
 def solution(bandage, health, attacks):
-    hp = health
-    for idx, atk in enumerate(attacks):        
-        # 공격 당함
-        dmg = atk[1]
-        hp -= dmg
-        print('공격 직후: ', hp)
-        if hp <= 0:
-            return -1
-        
-        # 다음 공격 전까지의 시간 구하기
-        if idx + 1 < len(attacks): # 마지막 인덱스 직전까지
-            cooltime = attacks[idx+1][0] - attacks[idx][0]
-            # 회복
-            count = 0
-            for i in range(cooltime-1):
-                count += 1
-                hp += bandage[1]
-                if count == bandage[0]:
-                    hp += bandage[2]
-                    count = 0
-                if hp > health:
-                    hp = health
-                print(count, '회복 완료 후: ', hp)
+    (t, x, y) = (bandage[0], bandage[1], bandage[2])
+    lastTime = 0
+    currHealth = health
     
-    return hp
+    for attack in attacks:
+        # 1. 연속 성공시간 구하기
+        successTime = attack[0] - lastTime - 1
+        lastTime = attack[0]
+        
+        # 2. 총 회복 체력 구하고, 회복 시키기
+        if successTime >= t:
+            additionalHP = successTime * x + y * (successTime // t)
+        else:
+            additionalHP = successTime * x
             
+        if currHealth + additionalHP > health:
+            currHealth = health
+        else:
+            currHealth += additionalHP
+        
+        # 3. 몬스터 공격 받고 죽는지 사는지 판단하기
+        if currHealth - attack[1] <= 0:
+            return -1
+        else:
+            currHealth -= attack[1]
+        print(currHealth, additionalHP)
+    return currHealth
