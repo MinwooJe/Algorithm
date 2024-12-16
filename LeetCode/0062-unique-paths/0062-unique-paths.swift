718cc9b317b891aa49fa51f0d52e56cd9e87feac
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-12-16 17:52:11 +0900

    Time: 0 ms (100.00%), Space: 17.6 MB (48.90%) - LeetHub

--- LeetCode/0062-unique-paths/0062-unique-paths.swift
+++ LeetCode/0062-unique-paths/0062-unique-paths.swift
@@ -1,21 +1,19 @@
 class Solution {
     func uniquePaths(_ m: Int, _ n: Int) -> Int {
         var memo = Array(repeating: Array(repeating: -1, count: n), count: m)
-
-        for i in 0..<m {
-            memo[i][0] = 1
-        }
-
-        for i in 0..<n {
-            memo[0][i] = 1
-        }
-
-        for row in 1..<m {
-            for col in 1..<n {
-                memo[row][col] = memo[row - 1][col] + memo[row][col - 1]
+        
+        func dp(_ row: Int, _ col: Int) -> Int {
+            if row <= 0 || col <= 0 {
+                return 1
             }
+            
+            if memo[row][col] == -1 {
+                memo[row][col] = dp(row - 1, col) + dp(row, col - 1)
+            }
+            
+            return memo[row][col]
         }
-
-        return memo[m - 1][n - 1]
+        
+        return dp(m - 1, n - 1)
     }
 }
