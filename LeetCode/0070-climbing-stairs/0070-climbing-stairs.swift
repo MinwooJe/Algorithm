d6ef6aeb40f753f580cbd50f7ad462c3be775582
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-12-16 16:38:50 +0900

    Time: 3 ms (5.51%), Space: 17.6 MB (17.04%) - LeetHub

--- LeetCode/0070-climbing-stairs/0070-climbing-stairs.swift
+++ LeetCode/0070-climbing-stairs/0070-climbing-stairs.swift
@@ -2,14 +2,11 @@ class Solution {
     var memo = [Int: Int]()
     
     func climbStairs(_ n: Int) -> Int {
-        if n == 1 {
-            return 1
-        }
-        if n == 2 {
-            return 2
+        if n == 1 || n == 2 {
+            return n
         }
         
-        if !memo.keys.contains(n) {
+        if memo[n] == nil {
             memo[n] = climbStairs(n - 1) + climbStairs(n - 2)
         }
         
