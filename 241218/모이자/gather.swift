f2ef4340383ad0e64f7569cd3b1c757179b99dba
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-12-18 11:09:13 +0900

    Title: 모이자, Time: 21ms, Memory: 1MB, Status: Runtime Error - CodeTree

--- 241218/모이자/gather.swift
+++ 241218/모이자/gather.swift
@@ -4,15 +4,16 @@ import Foundation
 let n = Int(readLine()!)!
 let home = readLine()!.components(separatedBy: " ").map { Int($0)! }
 
-var result = [Int]()
+var minDistance = 99999999999999
 
 for targetAddress in 0..<home.count {
     var moveDistanceSum = 0
+    
     for (address, numberOfPeople) in home.enumerated() {
         // 거리 * 사람 수
         moveDistanceSum += abs(targetAddress - address) * numberOfPeople
     }
-    result.append(moveDistanceSum)
+    minDistance = min(minDistance, moveDistanceSum)
 }
 
-print(result.min()!)
+print(minDistance)
