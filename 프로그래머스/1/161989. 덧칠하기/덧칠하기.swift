cb4d4713cd77bd269ced2d3f416241789cbb3617
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-12-19 14:25:06 +0900

    [level 1] Title: 덧칠하기, Time: 9.72 ms, Memory: 17.7 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.swift
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.swift
@@ -1,14 +1,18 @@
 import Foundation
 
+// n: 벽의 개수, m: 롤러의 길이
+// 출력: 페인트 칠 횟수
 func solution(_ n:Int, _ m:Int, _ section:[Int]) -> Int {
     var result = 0
-    var endOfPaintedIdx = 0
+    var beforePaintedWall = 0
     
-    for s in section {
-        if s > endOfPaintedIdx {
-            endOfPaintedIdx = s + m - 1
-            result += 1
+    for currentWall in section {
+        if beforePaintedWall >= currentWall {
+            continue
         }
+        
+        result += 1
+        beforePaintedWall = currentWall + m - 1
     }
     
     return result
