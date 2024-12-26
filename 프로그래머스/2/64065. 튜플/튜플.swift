85ba151aff787cf71e6dcde755f9a91dd048287e
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-12-26 21:02:38 +0900

    [level 2] Title: 튜플, Time: 234.76 ms, Memory: 20.7 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.swift
+++ 프로그래머스/2/64065. 튜플/튜플.swift
@@ -1,76 +1,13 @@
-import Foundation
-    /**
-    1. s 파싱 -> 딕셔너리에 각 집합 담기 - 키: 원소 개수, 값: 각 집합
-    2. 키 순서대로 딕셔너리 순회하면서 집합에 넣기
-    3. 현재 집합 - 이전 집합 = 현재 원소 -> result 배열에 append
-    */
-    
-    /**
-    s 파싱 까다로운 점
-    1. "{", ",", "}" 나오면 각각 어떤 액션?
-    2. 한자리 수가 아니라면? ex) {11, 123, 1234}
-    
-    s 파싱의 목표
-    1. 
-    1.1 {{11, 123}, {123}} -> { { 1 1 , 1 2 3 } , { 1 2 3 } }
-    1.2 {{4, 2, 3}, {3}, {2, 3}} -> { { 4 , 2 , 3 } , { 3 } , { 2 , 3}}
-        -> [1: [123], 2: [11, 123]]
-        -> [1: [3], 2: [2, 3], 3: [4, 2, 3]]
-    
-    var num = "" -> {} 내부의 , 나올 때 까지 num에 c 추가
-    {} 내부 , 나오면 num을 tempArray에 추가
-    } 나오면 tempArray를 wholeDict에 추가
-    */
 func solution(_ s:String) -> [Int] {
+    var components = [Int: Int]()
     
-    var result = [Int]()
-    var wholeDict = [Int: [String]]()
-    var tempArray = [String]()
-    var tempStr: String = ""
-    var isInner = false
+    s.split { !$0.isNumber }.map { Int(String($0))! }.forEach { components[$0, default: 0] += 1}
     
-    let s = Array(s)[1..<(s.count - 1)]
-    for (idx, c) in s.enumerated() {
-        let c = String(c)
-        if c == "{" {
-            isInner = true
-            continue
-        }
-        
-        if c == "," && isInner {
-            tempArray.append(tempStr)
-            tempStr = ""
-            continue
-        } else if c == "," {
-            continue
-        }
-        
-        if c == "}" {
-            tempArray.append(tempStr)
-            wholeDict[tempArray.count] = tempArray
-            isInner = false
-            tempStr = ""
-            tempArray = []
-            continue
-        }
-        
-        tempStr += c
-    }
-    
-    var beforeSet = Set<String>()
-    var currSet = Set<String>()
-    
-    for i in 1...wholeDict.count {
-        currSet = Set(wholeDict[i]!)
-        
-        let diff = currSet.subtracting(beforeSet)
-        
-        if let element = diff.first,
-           let value = Int(element) {
-               result.append(value)
-           }
-        beforeSet = currSet
-    }
-    
-    return result
+    return components.sorted { $0.value > $1.value }.map { $0.key }
 }
+
+/**
+다르게 생각하면, 튜플의 앞쪽 원소는 많이 나옴.
+예를 들어 테케 2의 2는 총 네 번 나옴. 4는 한 번 나옴.
+따라서 각 숫자가 몇 번 나왔는지 딕셔너리를 이용해 개수를 세고, 값의 내림차순으로 키를 리턴하면 됨.
+*/
