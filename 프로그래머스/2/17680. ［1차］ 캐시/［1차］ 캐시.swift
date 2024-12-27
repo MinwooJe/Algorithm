1a133cc4769cd9ddc749c3b8f172459f5661926d
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2024-12-27 12:14:57 +0900

    [level 2] Title: [1차] 캐시, Time: 430.41 ms, Memory: 21.9 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.swift
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.swift
@@ -1,24 +1,40 @@
+// cities: 10만 -> 시간 복잡도 주의
+/**
+LRU:
+가장 오래된 놈 제거
+재참조 -> 큐의 가장 뒤로
+*/
 func solution(_ cacheSize:Int, _ cities:[String]) -> Int {
-    var cache = [String]()
-    var executionTime = 0
+    let cache = Cache()
+    var result = 0
     
-    guard cacheSize != 0 else {
-        return cities.count * 5
-    }
+    guard cacheSize != 0 else { return cities.count * 5 }
     
     for city in cities {
         let city = city.lowercased()
-        if cache.contains(city) {
-            executionTime += 1
-            cache.remove(at: cache.firstIndex(of: city)!)
-        } else {
-            executionTime += 5
-            if cache.count >= cacheSize {
-                cache.removeFirst()
-            }
-        }
-        cache.append(city)
+        let executionTime = cache.caching(city, cacheSize)
+        result += executionTime
     }
     
-    return executionTime
+    return result
+}
+
+class Cache {
+    var cache = [String]()
+    
+    func caching(_ city: String, _ cacheSize: Int) -> Int {
+        if cache.contains(city) {
+            let idx = cache.firstIndex(of: city)!
+            let poppedCity = cache.remove(at: idx)
+            cache.append(poppedCity)
+            return 1
+        } else if cache.count < cacheSize {
+            cache.append(city)
+            return 5
+        } else {
+            cache.removeFirst()
+            cache.append(city)
+            return 5
+        }
+    }
 }
