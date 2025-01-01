bb9ac92594aca2e886f51b07740a23a27c4d4a3c
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-01-01 23:57:25 +0900

    [Bronze II] Title: 분해합, Time: 548 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.swift
+++ 백준/Bronze/2231. 분해합/분해합.swift
@@ -1,13 +1,11 @@
 let target = Int(readLine()!)!
-var i = 1
-var decomposition = 0
 var generator = target
 var generators = [Int]()
 
 while generator != 0 {
     generator -= 1
     let arr = String(generator).compactMap { $0.wholeNumberValue }
-    decomposition = generator + arr.reduce(0, +)
+    let decomposition = generator + arr.reduce(0, +)
     
     if decomposition == target {
         generators.append(generator)
