b8064df2c81416e6e7e1b1cfbd14359de1636d42
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-01-02 17:45:22 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/README.md
+++ 프로그래머스/2/12980. 점프와 순간 이동/README.md
@@ -1,10 +1,10 @@
 # [level 2] 점프와 순간 이동 - 12980 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12980?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12980) 
 
 ### 성능 요약
 
-메모리: 16.3 MB, 시간: 0.01 ms
+메모리: 16.4 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 18일 21:36:37
+2025년 01월 02일 17:45:20
 
 ### 문제 설명
 
--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.swift
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.swift
@@ -1,18 +1,24 @@
 import Foundation
+/**
+순간이동: 건전지 사용량 x
+k칸 점프: k만큼 건전지 사용
 
-func solution(_ n:Int) -> Int 
-{
-    var ans:Int = 0
+무조건 순간이동을 하는게 좋음.
+그런데 1부터 2를 곱해서 n까지 될 수 있냐 없냐는 판단하기 어려우므로 반대로 생각
+n을 2로 나누고, 홀수면 1을 뺌(점프)
+*/
+func solution(_ n:Int) -> Int {
     var n = n
+    var answer = 1
     
-    while n > 0 {
+    while n != 1 {
         if n % 2 == 0 {
             n /= 2
         } else {
             n -= 1
-            ans += 1
+            answer += 1
         }
     }
     
-    return ans
+    return answer
 }
