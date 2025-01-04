7c75b2d01776b79c4edac66afadd2b7d29120e49
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-01-04 12:54:02 +0900

    [Bronze II] Title: 분해합, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.swift
+++ 백준/Bronze/2231. 분해합/분해합.swift
@@ -1,7 +1,12 @@
 let target = Int(readLine()!)!
+var start = 1
 var decomposition = 0
 
-for i in 1...target {
+if target > 18 {
+    start = target - String(target).count * 9
+}
+
+for i in start..<target {
     let candidate = i + String(i).map { Int(String($0))! }.reduce(0, +)
     if candidate == target {
         decomposition = i
