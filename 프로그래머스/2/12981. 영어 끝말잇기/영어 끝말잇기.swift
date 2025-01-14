86dc80a1a5f6653319c3f9bbf1b74b363ea70c03
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-01-14 23:16:03 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.15 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.swift
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.swift
@@ -1,30 +1,25 @@
 import Foundation
 
+var wordDictionary = [String: Bool]()
 func solution(_ n:Int, _ words:[String]) -> [Int] {
-    let index = getFailIndex(n, words)
-    if index == 0 {
-        return [0, 0]
-    } else {
-        return [index % n + 1, index / n + 1]
+    wordDictionary[words[0]] = true
+
+    for idx in 1..<words.count {
+        if isDuplicated(words[idx]) || !isValid(words[idx - 1], words[idx]) {
+            let player = idx % n + 1
+            let round = idx / n + 1
+            return [player, round]
+        }
+        wordDictionary[words[idx]] = true
     }
+
+    return [0, 0]
 }
 
-func getFailIndex(_ n: Int, _ words: [String]) -> Int {
-    let wordsCount = words.count
-    var appearedWordHash = [words[0]: 1]
-    
-    for i in 0..<wordsCount - 1 {
-        appearedWordHash[words[i + 1], default: 0] += 1
-        
-        if appearedWordHash[words[i + 1]]! >  1 {
-            return i + 1
-        }
-        
-        if let frontWordsLast = words[i].last,
-           let backWordsFirst = words[i + 1].first,
-           frontWordsLast != backWordsFirst {
-               return i + 1
-        }
-    }
-    return 0
+func isDuplicated(_ word: String) -> Bool {
+    return wordDictionary[word] != nil
+}
+
+func isValid(_ prev: String, _ curr: String) -> Bool {
+    return prev.last == curr.first
 }
