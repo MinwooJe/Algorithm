82f09ed6727d86d2978718ec80d1e02d0bfa7da9
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-01-21 11:49:02 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.swift
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.swift
@@ -1,17 +1,10 @@
 import Foundation
-/**
-순간이동: 건전지 사용량 x
-k칸 점프: k만큼 건전지 사용
 
-무조건 순간이동을 하는게 좋음.
-그런데 1부터 2를 곱해서 n까지 될 수 있냐 없냐는 판단하기 어려우므로 반대로 생각
-n을 2로 나누고, 홀수면 1을 뺌(점프)
-*/
 func solution(_ n:Int) -> Int {
+    var answer = 0
     var n = n
-    var answer = 1
-    
-    while n != 1 {
+
+    while n > 0 {
         if n % 2 == 0 {
             n /= 2
         } else {
@@ -19,6 +12,6 @@ func solution(_ n:Int) -> Int {
             answer += 1
         }
     }
-    
+
     return answer
 }
