36d2af1dd77b9fc46ea5cd9a3ce50e938a357e27
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-01-23 23:54:49 +0900

    [level 2] Title: 기능개발, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.swift
+++ 프로그래머스/2/42586. 기능개발/기능개발.swift
@@ -1,28 +1,5 @@
 import Foundation
 
-// progresses: 작업의 진도 - 100이 되어야 배포, 순서는 먼재 배포해야 할 순서
-// speeds: 각 작업이 개발 속도
-// 각 배포마다 몇 개의기능이 배포되는지?
 func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
-    var completedDay = [Int]()
-    var result = [Int]()
-
-    for i in 0..<progresses.count {
-        let day = (100 - progresses[i] - 1) / speeds[i] + 1
-        completedDay.append(day)
-    }
-
-    // 하루 단위
-    while !completedDay.isEmpty {
-        var count = 1
-        var todayTask = completedDay.removeFirst()
-        // 하루 동안 배포 할 수 있는 작업 구하기
-        while !completedDay.isEmpty && completedDay[0] <= todayTask {
-            completedDay.removeFirst()
-            count += 1
-        }
-        result.append(count)
-    }
-    
-    return result
+    return []
 }
