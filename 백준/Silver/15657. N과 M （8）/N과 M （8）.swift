576e0a1288ce72d2a874a653ec54dfe9c8dd8b98
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-02-15 13:20:00 +0900

    [Silver III] Title: N과 M (8), Time: 24 ms, Memory: 79516 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.swift
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.swift
@@ -1,33 +1,28 @@
-// n개의 수 중에서, 길이가 m인 중복 비내림차순 수열 만들기
-
+import Foundation
+/**
+ 중복 조합 구하기
+ 조합의 각 원소의 크기는 m
+ 비내림차순 = 크거나 같다
+ 출력 형식은 한 줄에 하나씩
+ */
 let input = readLine()!.split(separator: " ").map { Int($0)! }
-let (n, m) = (input[0], input[1])
 let nums = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
+let (n, m) = (input[0], input[1])
 
-func getSequence(_ m: Int, _ nums: [Int]) -> [[Int]] {
-    var result = [[Int]]()
-    
+func combination(_ nums: [Int], _ count: Int) {
     func bruteforce(_ start: Int, _ curr: [Int]) {
-        if curr.count == m {
-            result.append(curr)
+        if curr.count == count {
+            print(curr.map { String($0) }.joined(separator: " "))
             return
         }
         
-        var curr = curr
         for i in start..<n {
-            curr.append(nums[i])
-            bruteforce(i, curr)
-            curr.removeLast()
+            bruteforce(i, curr + [nums[i]])
         }
     }
     
     bruteforce(0, [])
-    return result
+
 }
 
-let result = getSequence(m, nums)
-
-result.forEach { r in
-    r.forEach { print($0, terminator: " ")}
-    print()
-}
+combination(nums, m)
