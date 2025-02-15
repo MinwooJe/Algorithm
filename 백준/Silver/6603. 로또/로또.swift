ff59db399f14fa36511c467b02d39bf57422d6b5
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-02-15 13:46:19 +0900

    [Silver II] Title: 로또, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.swift
+++ 백준/Silver/6603. 로또/로또.swift
@@ -1,33 +1,22 @@
 while true {
     let input = readLine()!.split(separator: " ").map { Int($0)! }
     guard input != [0] else { break }
-    let (k, set) = (input[0], Array(input[1..<input.count]))
-    let result = subset(k, set)
-    print(parseToString(from: result))
+    let (k, nums) = (input[0], Array(input[1..<input.count]))
+    combination(nums, k)
     print()
 }
 
-func subset(_ k: Int, _ nums: [Int]) -> [[Int]] {
-    var result = [[Int]]()
-    
+func combination(_ nums: [Int], _ k: Int) {
     func bruteforce(_ start: Int, _ curr: [Int]) {
         if curr.count == 6 {
-            result.append(curr)
+            print(curr.map { String($0) }.joined(separator: " "))
             return
         }
         
-        var curr = curr
-        for i in start..<k {
-            curr.append(nums[i])
-            bruteforce(i + 1, curr)
-            curr.removeLast()
+        for i in start..<nums.count {
+            bruteforce(i + 1, curr + [nums[i]])
         }
     }
     
     bruteforce(0, [])
-    return result
-}
-
-func parseToString(from arr: [[Int]]) -> String {
-    return arr.map { $0.map { String($0) }.joined(separator: " ") }.joined(separator: "\n")
 }
