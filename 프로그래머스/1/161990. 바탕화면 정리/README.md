82e791928ac97ae4211a449eaade273e35eb89ed
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-02-17 17:22:58 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.27 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/README.md
+++ 프로그래머스/1/161990. 바탕화면 정리/README.md
@@ -1,10 +1,10 @@
 # [level 1] 바탕화면 정리 - 161990 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161990?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161990) 
 
 ### 성능 요약
 
-메모리: 16 MB, 시간: 1.42 ms
+메모리: 16.3 MB, 시간: 0.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 14일 15:20:05
+2025년 02월 17일 17:22:56
 
 ### 문제 설명
 
--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.swift
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.swift
@@ -1,16 +1,13 @@
 import Foundation
 
+/**
+빈칸: ., 파일: #
+*/
 func solution(_ wallpaper:[String]) -> [Int] {
     let wallpaper = wallpaper.map { Array($0) }
-    // 위, 왼쪽 -> 최솟값, 아래, 오른쪽 -> 최댓값
-    // 위: 행의 최솟값, 왼쪽: 열의 최솟값
-        // #의 row, col 구하면 됨.
-    // 아래: 행의 최댓값, 오른쪽: 열의 최댓값
-        // #의 row + 1, col + 1 구하면 됨.
-    // lux: 행의 최솟값, luy: 열의 최솟값, rdx: 행의 최댓값 + 1, rdy: 열의 최댓값 + 1
-    let MAX = max(wallpaper.count, wallpaper[0].count)
-    var (lux, luy, rdx, rdy) = (MAX, MAX, 0, 0)
-
+    var (lux, luy) = (Int.max, Int.max)     // 최소값 찾아야 됨.
+    var (rdx, rdy) = (Int.min, Int.min)     // 최댓값 찾아야 함.
+    
     for row in 0..<wallpaper.count {
         for col in 0..<wallpaper[0].count {
             if wallpaper[row][col] == "#" {
@@ -21,6 +18,6 @@ func solution(_ wallpaper:[String]) -> [Int] {
             }
         }
     }
-    
+
     return [lux, luy, rdx, rdy]
 }
