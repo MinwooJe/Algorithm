4a4ebf22c25cbf02640b7fec38030043a73c96a7
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-02-24 10:07:55 +0900

    [level 2] Title: 스킬트리, Time: 0.12 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.swift
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.swift
@@ -1,35 +1,21 @@
 import Foundation
-/**
-백트래킹?
-skill_trees 순회
-skillSet, skillStack
-if skillSet.contains(c) in skill_tree {
-    if skillStack[-1] == c {
-        stack.pop
-}
-}
-*/
+
 func solution(_ skill:String, _ skill_trees:[String]) -> Int {
     var result = 0
-
-    for skill_tree in skill_trees {
+    
+    for tree in skill_trees {
+        var skill = Array(skill)
         var flag = true
-        let skillSet = Set(skill)
-        var skillStack = Array(skill.reversed())
-
-        for skill in skill_tree {
-            if skillSet.contains(skill) {
-                if skill == skillStack[skillStack.count - 1] {
-                    skillStack.removeLast()
-                    continue
-                } else {
+        for s in tree {
+            if skill.contains(s) && !skill.isEmpty {
+                guard s == skill[0] else {
                     flag = false
                     break
                 }
+                skill.removeFirst()
             }
         }
         result += flag ? 1 : 0
     }
-
     return result
 }
