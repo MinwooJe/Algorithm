a605cfbb30e55f9beeed632cab2df42703f00be5
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-02-24 12:09:33 +0900

    [level 2] Title: 택배상자, Time: 292.37 ms, Memory: 60.6 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/README.md
+++ 프로그래머스/2/131704. 택배상자/README.md
@@ -1,10 +1,10 @@
 # [level 2] 택배상자 - 131704 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131704) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131704#) 
 
 ### 성능 요약
 
-메모리: 62.6 MB, 시간: 487.51 ms
+메모리: 60.6 MB, 시간: 292.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 23일 14:24:02
+2025년 02월 24일 12:09:31
 
 ### 문제 설명
 
--- 프로그래머스/2/131704. 택배상자/택배상자.swift
+++ 프로그래머스/2/131704. 택배상자/택배상자.swift
@@ -1,40 +1,23 @@
 import Foundation
 
 func solution(_ order:[Int]) -> Int {
-    var target = 0
     var stack = [Int]()
-    var result = 0
-
-    for num in 1...order.count {
-        if order[target] != num {
-            if stack.isEmpty {
-                stack.append(num)
-                continue
-            }
-            
-            while !stack.isEmpty && stack.last! == order[target] {
-                target += 1
-                result += 1
-                stack.removeLast()
-            }
-
-            if stack.last != order[target] {
-                stack.append(num)
-                continue
-            }
-        }
-
-        if num == order[target] {
-            result += 1
-            target += 1
-        }
-    }
-
-    while !stack.isEmpty && stack.last! == order[target] {
-        target += 1
-        result += 1
-        stack.removeLast()
-    }
+    var number = 1
+    var answer = 0
     
-    return result
+    for o in order {
+        while number <= o {
+            stack.append(number)
+            number += 1
+        }
+        
+        if stack.last! == o {
+            stack.removeLast()
+            answer += 1
+        } else {
+            break
+        }
+    }
+
+    return answer
 }
