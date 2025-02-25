d7f6d8d3174b8e9e8561672787533efc61ca16f6
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-02-25 15:21:00 +0900

    [level 2] Title: 택배상자, Time: 306.23 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/README.md
+++ 프로그래머스/2/131704. 택배상자/README.md
@@ -1,10 +1,10 @@
 # [level 2] 택배상자 - 131704 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131704#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131704) 
 
 ### 성능 요약
 
-메모리: 60.6 MB, 시간: 292.37 ms
+메모리: 60.7 MB, 시간: 306.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 24일 12:09:31
+2025년 02월 25일 15:20:56
 
 ### 문제 설명
 
--- 프로그래머스/2/131704. 택배상자/택배상자.swift
+++ 프로그래머스/2/131704. 택배상자/택배상자.swift
@@ -3,21 +3,20 @@ import Foundation
 func solution(_ order:[Int]) -> Int {
     var stack = [Int]()
     var number = 1
-    var answer = 0
-    
+    var result = 0
+
     for o in order {
         while number <= o {
             stack.append(number)
             number += 1
         }
-        
+
         if stack.last! == o {
             stack.removeLast()
-            answer += 1
+            result += 1
         } else {
             break
         }
     }
-
-    return answer
+    return result
 }
