b69e41a68ebba4a559b18b2c15929b139692f4eb
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-03-05 21:16:44 +0900

    [level 2] Title: 행렬의 곱셈, Time: 23.91 ms, Memory: 17.7 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.swift
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.swift
@@ -1,17 +1,19 @@
 import Foundation
 
 func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
-    var result = Array(
-        repeating: Array(repeating: 0, count: arr2[0].count), 
-        count: arr1.count
-    )
+    let rowCount = arr1.count
+    let colCount = arr2[0].count
+    var result = Array(repeating: Array(repeating: 0, count: colCount), count: rowCount)
     
-    for row in 0..<arr1.count {
-        for col in 0..<arr2[0].count {
-            result[row][col] = (0..<arr1[0].count)
-                .map { arr1[row][$0] * arr2[$0][col] }
-                .reduce(0, +)
+    for rowIdx in 0..<rowCount {
+        for colIdx in 0..<colCount {
+            var sum = 0
+            for i in 0..<arr2.count {
+                sum += arr1[rowIdx][i] * arr2[i][colIdx]
+            }
+            result[rowIdx][colIdx] = sum
         }
     }
+    
     return result
 }
