27e81c65d2e19005bab77e77c95b676270a63b04
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-03-06 13:52:53 +0900

    [level 2] Title: 의상, Time: 0.06 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.swift
+++ 프로그래머스/2/42578. 의상/의상.swift
@@ -3,14 +3,14 @@ import Foundation
 func solution(_ clothes:[[String]]) -> Int {
     var clothesHash = [String: Int]()
     var result = 1
-    
-    for clothe in clothes {
-        clothesHash[clothe[1], default: 0] += 1
+
+    for cloth in clothes {
+        clothesHash[cloth[1], default: 0] += 1
     }
     
-    for k in clothesHash.values {
-        result *= (k + 1)
+    for count in clothesHash.values {
+        result *= (count + 1)
     }
-    
+
     return result - 1
 }
