5ea41fc2943d12a487670c9664804bfdb849d0b9
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-03-06 15:49:10 +0900

    [level 2] Title: 기능개발, Time: 0.15 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.swift
+++ 프로그래머스/2/42586. 기능개발/기능개발.swift
@@ -1,5 +1,34 @@
 import Foundation
 
 func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
-    return []
+    var result = [Int]()
+    var queue = [Int]()     // 남은 작업 일자
+    
+    for (p, s) in zip(progresses, speeds) {
+        let remain: Int
+        if (100 - p) % s == 0 {
+            remain = (100 - p) / s
+        } else {
+            remain = (100 - p) / s + 1
+        }
+        queue.append(remain)
+    }
+
+    while !queue.isEmpty {
+        let finishedTask = queue.removeFirst()
+        var finishedCount = 1
+        
+        while true {
+            guard !queue.isEmpty else { break }
+            if finishedTask >= queue[0] {
+                queue.removeFirst()
+                finishedCount += 1
+            } else {
+                break
+            }
+        }
+        result.append(finishedCount)
+    }
+
+    return result
 }
