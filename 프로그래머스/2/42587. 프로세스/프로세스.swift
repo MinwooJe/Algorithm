0f6a5fb04b6fcfc9d89b09183f12f5885e9d9fd6
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-03-13 19:50:37 +0900

    [level 2] Title: 프로세스, Time: 11.22 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.swift
+++ 프로그래머스/2/42587. 프로세스/프로세스.swift
@@ -1,23 +1,24 @@
 import Foundation
 
+// queue.pop == 실행
 func solution(_ priorities:[Int], _ location:Int) -> Int {
-    var prioritiesWithIdx = [(p: Int, idx: Int)]()
-    var count = 0
-    
-    for (idx, priority) in priorities.enumerated() {
-        prioritiesWithIdx.append((priority, idx))
+    var queue = [(Int, Int)]()  // (프로세스 인덱스, 우선순위)
+    var result = 0
+    for i in 0..<priorities.count {
+        queue.append((i, priorities[i]))
     }
-    
+
     while true {
-        let process = prioritiesWithIdx.removeFirst()
-        
-        if prioritiesWithIdx.filter { $0.p > process.p }.count > 0 {
-            prioritiesWithIdx.append(process)
+        let process = queue.removeFirst()
+        if queue.filter { $0.1 > process.1 }.count > 0 {    // 우선순위 안됨.
+            queue.append(process)
         } else {
-            count += 1
-            guard process.idx != location else { break }
+            result += 1
+            if process.0 == location {  // 우선순위 조건 충족 -> 실행
+                break
+            }
         }
     }
-    
-    return count
+
+    return result
 }
