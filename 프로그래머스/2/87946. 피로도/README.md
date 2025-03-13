adb42202b6a1df9c31a375daea62c9c83a49129c
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-03-13 22:04:49 +0900

    [level 2] Title: 피로도, Time: 119.35 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
 
 ### 성능 요약
 
-메모리: 16.3 MB, 시간: 32.63 ms
+메모리: 16.5 MB, 시간: 119.35 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 26일 18:59:10
+2025년 03월 13일 22:04:43
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.swift
+++ 프로그래머스/2/87946. 피로도/피로도.swift
@@ -1,22 +1,26 @@
 import Foundation
+/**
+1. 최소 필요 피로도가 낮은 순으로 정렬하면 되는거 아님?
+-> ㄴㄴ 소모 피로도도 중요함. 내 체력이 있어야 던전을 더 탐색할 수 있으니
+-> 그냥 완탐해야겠는데?
 
-// k: 유저의 현재 피로도, dungeons: [던전 별 최소 필요 피로도, 소모 피로도]
-// 유저가 탐험할 수 있는 최대 던전 수
+2. 백트래킹
+백트랙 기준: 최소 필요 피로도 > 현재 피로도
+방문한 던전은 방문 안해: curr에 없으면 됨.
+*/
 func solution(_ k:Int, _ dungeons:[[Int]]) -> Int {
-    var maxCount = 0
+    var currHp = k
+    var result = 0
     
-    func backtrack(_ k: Int, _ count: Int, _ visited: [Bool]) {
-        maxCount = max(maxCount, count)
+    func backtrack(_ currHp: Int, _ curr: [Int]) {
+        result = max(result, curr.count)
         
         for i in 0..<dungeons.count {
-            if !visited[i] && k >= dungeons[i][0] {
-                var newVisited  = visited
-                newVisited[i] = true
-                backtrack(k - dungeons[i][1], count + 1, newVisited)
-            }
+            guard currHp >= dungeons[i][0] && !curr.contains(i) else { continue }
+            backtrack(currHp - dungeons[i][1], curr + [i])
         }
     }
     
-    backtrack(k, 0, Array(repeating: false, count: dungeons.count))
-    return maxCount
+    backtrack(currHp, [])
+    return result
 }
