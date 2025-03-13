92833f0b11037a5daad5c36c841576405b1a6f47
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-03-13 20:10:36 +0900

    [level 2] Title: 피보나치 수, Time: 2.79 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/README.md
+++ 프로그래머스/2/12945. 피보나치 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피보나치 수 - 12945 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945) 
 
 ### 성능 요약
 
-메모리: 16.4 MB, 시간: 3.80 ms
+메모리: 16.4 MB, 시간: 2.79 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 31일 19:49:25
+2025년 03월 13일 20:10:34
 
 ### 문제 설명
 
--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.swift
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.swift
@@ -1,10 +1,10 @@
 func solution(_ n:Int) -> Int {
-    var table = Array(repeating: 0, count: n + 1)
-    (table[0], table[1]) = (0, 1)
+    var dp = Array(repeating: -1, count: n + 1)
+    (dp[0], dp[1]) = (0, 1)
     
     for i in 2...n {
-        table[i] = (table[i - 1] + table[i - 2]) % 1234567
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567
     }
-
-    return table[n]
+    
+    return dp[n]
 }
