8848e1f6fa80b0995b04c56a7d5713a2f0ef55ee
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-03-13 21:50:46 +0900

    [level 2] Title: 택배상자, Time: 349.67 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.swift
+++ 프로그래머스/2/131704. 택배상자/택배상자.swift
@@ -1,16 +1,19 @@
 import Foundation
 
+// 기사님한테 넘겨 줄 물건도 스택에 쌓기: parcel <= o
+// 그러면 스택만 봐도 됨.
+// 스택의 탑과 o가 다르면 그냥 끗
 func solution(_ order:[Int]) -> Int {
     var stack = [Int]()
-    var number = 1
+    var parcel = 1
     var result = 0
-
+    
     for o in order {
-        while number <= o {
-            stack.append(number)
-            number += 1
+        while parcel <= o {
+            stack.append(parcel)
+            parcel += 1
         }
-
+        
         if stack.last! == o {
             stack.removeLast()
             result += 1
@@ -18,5 +21,6 @@ func solution(_ order:[Int]) -> Int {
             break
         }
     }
+    
     return result
 }
