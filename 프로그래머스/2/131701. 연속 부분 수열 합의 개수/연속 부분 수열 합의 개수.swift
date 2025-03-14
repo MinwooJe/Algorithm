9363621739c193dce08eff11ee82571af78d72e6
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-03-14 10:34:28 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 179.68 ms, Memory: 27.6 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.swift
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.swift
@@ -1,22 +1,14 @@
-import Foundation
-/**
-길이가 1부터 n까지
-    start
-        start부터 end까지
-
-
-*/
 func solution(_ elements:[Int]) -> Int {
-    let circular = elements + elements
-    var totalSum = Set<Int>()
-    
-    for start in 0..<elements.count {
-        var num = 0
-        for i in 0..<elements.count {
-            num += circular[start + i]
-            totalSum.insert(num)
+    let seq = elements + elements
+    var sum = Set<Int>()
+
+    for i in 0..<elements.count {
+        var tempSum = 0
+        for j in i..<i + elements.count {
+            tempSum += seq[j]
+            sum.insert(tempSum)
         }
     }
     
-    return totalSum.count
+    return sum.count
 }
