232052c1661337d95148a678ebed8603f35ff82f
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-03-14 11:45:30 +0900

    [level 2] Title: n^2 배열 자르기, Time: 60.58 ms, Memory: 35.2 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.swift
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.swift
@@ -1,13 +1,10 @@
 import Foundation
-
+/**
+2차원 배열을 1차원 배열로 만들었을 때, 1차원 배열의 인덱스 k라고 하면
+k = row * n + col 관계 성립.
+row와 col과 진짜 숫자의 관계는 둘 중 max + 1.
+따라서 k -> row, col -> num 과정을 거쳐 num을 찾으면 됨.
+*/
 func solution(_ n:Int, _ left:Int64, _ right:Int64) -> [Int] {
-    var result = [Int]()
-
-    for num in Int(left)...Int(right) {
-        let row = num / n
-        let col = num % n
-        result.append(max(row, col) + 1)
-    }
-
-    return result
+    return (Int(left)...Int(right)).map { max($0 / n, $0 % n) + 1 }
 }
