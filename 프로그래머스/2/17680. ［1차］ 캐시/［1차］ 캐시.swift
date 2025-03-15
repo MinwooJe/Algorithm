8c5644f43deb9315cc76fb11d198ae204e6bcaf7
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-03-15 12:23:33 +0900

    [level 2] Title: [1차] 캐시, Time: 426.33 ms, Memory: 21.7 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.swift
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.swift
@@ -1,40 +1,39 @@
-// cities: 10만 -> 시간 복잡도 주의
-/**
-LRU:
-가장 오래된 놈 제거
-재참조 -> 큐의 가장 뒤로
-*/
+// 도시 이름 입력 받고, 캐시 저장, LRU 사용.
+// hit: 1, miss: 5
+// 주의: 도시 이름 대소문자 구분 x
+    /**
+    1. 캐시에 없다. -> 캐시에 추가
+        1.1 캐시 꽉 찼다. -> removeFirst(), append
+        1.2 캐시 자리 남았다. -> append()
+        result += 5
+    2. 캐시에 있다. -> result += 1
+        2.1 캐시의 맨 뒤에 있다. -> continue
+        2.2 else -> 해당 인덱스 찾아서 삭제 -> append
+    */
 func solution(_ cacheSize:Int, _ cities:[String]) -> Int {
-    let cache = Cache()
+    guard cacheSize != 0 else { return 5 * cities.count }
+    var cache = [String]()      // cacheSize가 30이하이므로 배열도 성능 크게 상관 없을 듯.
     var result = 0
-    
-    guard cacheSize != 0 else { return cities.count * 5 }
     
     for city in cities {
         let city = city.lowercased()
-        let executionTime = cache.caching(city, cacheSize)
-        result += executionTime
-    }
-    
-    return result
-}
-
-class Cache {
-    var cache = [String]()
-    
-    func caching(_ city: String, _ cacheSize: Int) -> Int {
         if cache.contains(city) {
-            let idx = cache.firstIndex(of: city)!
-            let poppedCity = cache.remove(at: idx)
-            cache.append(poppedCity)
-            return 1
-        } else if cache.count < cacheSize {
-            cache.append(city)
-            return 5
+            if !cache.isEmpty && cache[cache.count - 1] != city {
+                let idx = cache.firstIndex(of: city)!
+                cache.remove(at: idx)
+                cache.append(city)
+            }
+            result += 1
         } else {
-            cache.removeFirst()
-            cache.append(city)
-            return 5
+            if cache.count == cacheSize {
+                cache.removeFirst()
+                cache.append(city)
+            } else {
+                cache.append(city)
+            }
+            result += 5
         }
     }
+
+    return result
 }
