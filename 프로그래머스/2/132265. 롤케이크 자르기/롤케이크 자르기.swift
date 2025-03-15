8db76e18567b7987a6ca7710e5850b21aca4f0f2
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-03-15 14:15:18 +0900

    [level 2] Title: 롤케이크 자르기, Time: 871.91 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.swift
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.swift
@@ -1,22 +1,22 @@
 import Foundation
 
 func solution(_ topping:[Int]) -> Int {
-    var result = 0
+    var rightTopping = [Int: Int]()
     var leftTopping = Set<Int>()
-    var rightTopping = [Int: Int]()     // [토핑 종류: 개수]
-    
+    var result = 0
+
     for t in topping {
         rightTopping[t, default: 0] += 1
     }
-
+    
     for t in topping {
-        rightTopping[t]! -= 1
+        rightTopping[t, default: 0] -= 1
         leftTopping.insert(t)
-        
+
         if rightTopping[t, default: 0] <= 0 {
             rightTopping[t] = nil
         }
-
+        
         if leftTopping.count == rightTopping.count {
             result += 1
         }
