3e04dbcab97fce80d51765ebec00a8f03d68658f
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-03-18 12:48:28 +0900

    [Gold V] Title: 토마토, Time: 264 ms, Memory: 110224 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.swift
+++ 백준/Gold/7576. 토마토/토마토.swift
@@ -1,62 +1,63 @@
+/**
+ bfs: O(NM)임. 1인 곳마다 bfs를 돌리면 O((NM)^2)이 걸림.
+ -> 큐를 전역 변수로 선언하고, 1인 좌표를 큐에 넣으면 됨.
+ */
+
 let input = readLine()!.split(separator: " ").map { Int($0)! }
 let (colCount, rowCount) = (input[0], input[1])
+let dRow = [-1, 1, 0, 0]
+let dCol = [0, 0, -1, 1]
+
+var dist = Array(repeating: Array(repeating: 0, count: colCount), count: rowCount)
 var board = [[Int]]()
+var queue = [(Int, Int)]()
 
 for _ in 0..<rowCount {
     let input = readLine()!.split(separator: " ").map { Int($0)! }
     board.append(input)
 }
 
-let dRow = [-1, 1, 0, 0]
-let dCol = [0, 0, -1, 1]
-var dayBoard = board
-var queue = [(Int, Int, Int)]()
-
-func bfs() -> Int {
-    var totalDay = 0
-    var visited = Array(repeating: Array(repeating: false, count: colCount), count: rowCount)
-    var queueIndex = 0
-
-    while queueIndex < queue.count {
-        let (curRow, curCol, count) = queue[queueIndex]
-        queueIndex += 1
-
-        for i in 0..<4 {
-            let nextRow = curRow + dRow[i]
-            let nextCol = curCol + dCol[i]
-            
-            if nextRow >= 0 && nextRow < rowCount && nextCol >= 0 && nextCol < colCount {
-                if board[nextRow][nextCol] == 0 && visited[nextRow][nextCol] == false {
-                    queue.append((nextRow, nextCol, count + 1))
-                    visited[nextRow][nextCol] = true
-                    if dayBoard[nextRow][nextCol] == 0 {
-                        dayBoard[nextRow][nextCol] = count + 1
-                    } else {
-                        dayBoard[nextRow][nextCol] = min(dayBoard[nextRow][nextCol], count + 1)
-                    }
-                    totalDay = max(totalDay, dayBoard[nextRow][nextCol])
-                }
-            }
-        }
-    }
-
-    return totalDay
-}
-
 for row in 0..<rowCount {
     for col in 0..<colCount {
         if board[row][col] == 1 {
-            queue.append((row, col, 0))
+            queue.append((row, col))
+        }
+        if board[row][col] == 0 {
+            dist[row][col] = -1
         }
     }
 }
 
-let result = bfs()
+var index = 0
+while index < queue.count {
+    let (currRow, currCol) = queue[index]
+    index += 1
+    
+    for i in 0..<4 {
+        let nextRow = currRow + dRow[i]
+        let nextCol = currCol + dCol[i]
+        
+        if nextRow < 0 || nextRow >= rowCount || nextCol < 0 || nextCol >= colCount { continue }
+        if dist[nextRow][nextCol] >= 0 { continue }
+        
+        dist[nextRow][nextCol] = dist[currRow][currCol] + 1
+        queue.append((nextRow, nextCol))
+    }
+}
 
-let notRipeCount = dayBoard.reduce(0) { $0 + $1.filter { $0 == 0 }.count } > 0
+var answer = 0
+var isAllRiped = true
+for row in 0..<rowCount {
+    for col in 0..<colCount {
+        if dist[row][col] == -1 {
+            isAllRiped = false
+        }
+        answer = max(answer, dist[row][col])
+    }
+}
 
-if notRipeCount {
-    print(-1)
+if isAllRiped {
+    print(answer)
 } else {
-    print(result)
+    print(-1)
 }
