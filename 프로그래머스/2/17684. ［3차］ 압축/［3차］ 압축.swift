d2a1f12a6e3b709a772a9048053b1c2d6177a2a4
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-03-20 18:14:34 +0900

    [level 2] Title: [3차] 압축, Time: 1.17 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.swift
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.swift
@@ -1,32 +1,26 @@
 func solution(_ msg:String) -> [Int] {
-    var msg = msg.map { String($0) }
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".map { String($0) }
+    var dict = Dictionary(uniqueKeysWithValues: zip(alphabet, (1...26)))
+    var msg = Array(msg).map { String($0) }
     var result = [Int]()
-    var dict = [String: Int]()
     
-    for (idx, char) in "ABCDEFGHIJKLMNOPQRSTUVWXYZ".enumerated() {
-        dict[String(char)] = idx + 1
-    }
-    
-    var currIdx = 0
-    var currWord = msg[0]
-    while currIdx < msg.count - 1 { // 조건 한 번 확인 필요
-        // 한 글자는 무조건 잇음.
-        // 사전에 있는지 검사
-            // 있으면?: 글자 붙여야 됨. (없을 때 까지)
-            // 없으면?: next와 합쳐서 사전에 저장, result에 추가 -> currWord, next 구분 필요
-                // currWord 초기화도 필요
-        let next = msg[currIdx + 1]
-        
-        if dict[currWord + next] == nil {
-            dict[currWord + next] = dict.count + 1
-            result.append(dict[currWord]!)
-            currWord = ""
+    var cur = msg[0]
+    var idx = 1
+    while idx < msg.count {
+        let next = msg[idx]
+        if dict[cur + next] != nil {
+            cur += next
+        } else {
+            result.append(dict[cur]!)
+            dict[cur + next] = dict.count + 1
+            cur = next
         }
-
-        currWord += next
-        currIdx += 1
+        idx += 1
     }
     
-    result.append(dict[currWord, default: 0])
+    if dict[cur] != nil {
+        result.append(dict[cur]!)
+    }
+    
     return result
 }
