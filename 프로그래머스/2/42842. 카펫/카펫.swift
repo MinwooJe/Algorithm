32429d161075e76d8ea17801ed5d8b1cc7ec0dd7
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-03-20 16:26:07 +0900

    [level 2] Title: 카펫, Time: 0.11 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.swift
+++ 프로그래머스/2/42842. 카펫/카펫.swift
@@ -1,34 +1,29 @@
 import Foundation
-
-// yellow의 개수는 넓이를 의미.
-// 이 문제는 카펫의 가로 세로 길이를 구해야되고, 각 구역은 직사각형이니
-// yellow의 약수가 결국 가로, 세로 길이의 후보들.
-// 얘네로 올바른 넓이를 만들 수 있는지 확인하면 됨.
+/**
+격자의 수 == 넓이
+직사각형의 넓이의 약수 짝 -> 가로, 세로
+(y_w + y_h) * 2 + 4(= 모서리 네 개) == brown의 넓이
+*/
 func solution(_ brown:Int, _ yellow:Int) -> [Int] {
-    var result = [0, 0]
-    if yellow * 2 + 6 == brown {    // 노란색이 한 줄일 경우
-        result = [yellow + 2, 3]
-    } else {
-        let pairOfDivisors = getPairOfDivisors(yellow)
-        for (w, h) in pairOfDivisors {
-            if (w + h) * 2 + 4 == brown {   // 올바른 카펫 모양일 경우
-                result = [w + 2, h + 2]
-                break
-            }
+    let pairOfDivisors = getPairOfDivisors(yellow)
+    var result = [Int]()
+    
+    for (w, h) in pairOfDivisors {
+        if (w + h) * 2 + 4 == brown {
+            result = [w + 2, h + 2]
         }
     }
     
     return result
 }
 
-/// return value: [(width, height)]
-func getPairOfDivisors(_ yellow: Int) -> [(Int, Int)] {
-    let end = Int(sqrt(Double(yellow)))
+func getPairOfDivisors(_ num: Int) -> [(w: Int, h: Int)] {
+    let end = Int(sqrt(Double(num)))
     var result = [(Int, Int)]()
     
     for i in 1...end {
-        if yellow % i == 0 {
-            result.append((yellow / i, i))
+        if num % i == 0 {
+            result.append((num / i, i))
         }
     }
     
