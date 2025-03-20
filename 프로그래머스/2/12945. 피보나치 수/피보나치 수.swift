25027e12403257c4a3df342496a959ab1d44709c
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-03-20 16:03:59 +0900

    [level 2] Title: 피보나치 수, Time: 2.75 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.swift
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.swift
@@ -1,6 +1,6 @@
 func solution(_ n:Int) -> Int {
     var table = Array(repeating: 0, count: n + 1)
-    (table[0], table[1], table[2]) = (0, 1, 2)
+    table[1] = 1
     
     for i in 2...n {
         table[i] = (table[i - 1] + table[i - 2]) % 1234567
