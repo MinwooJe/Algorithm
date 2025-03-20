9b03951d5428cdc9e043f62349d7d57c8aac7a30
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-03-20 18:29:43 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 530.82 ms, Memory: 184 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.swift
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.swift
@@ -1,16 +1,20 @@
 import Foundation
 
 func solution(_ numbers:[Int]) -> [Int] {
-    var stack = [(num: Int, idx: Int)]()
-    var result = Array(repeating: -1, count: numbers.count)
+    var stack = [(value: Int, idx: Int)]()
+    var result = Array(repeating: 0, count: numbers.count)
     
     for (idx, num) in numbers.enumerated() {
-        while !stack.isEmpty && stack[stack.count - 1].num < num {
-            let (_, i) = stack.removeLast()
-            result[i] = num
+        while !stack.isEmpty && stack[stack.count - 1].value < num {
+            let (_, idx) = stack.removeLast()
+            result[idx] = num
         }
-        
         stack.append((num, idx))
+    }
+    
+    while !stack.isEmpty {
+        let (_, idx) = stack.removeLast()
+        result[idx] = -1
     }
     
     return result
