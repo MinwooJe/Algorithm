1710ff0578811b929d2e2686940200d30219bffc
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-03-20 16:56:27 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 24.89 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.swift
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.swift
@@ -1,15 +1,12 @@
 import Foundation
-// 조건에 맞는 수들 중 소수 찾기
-// 조건: 0 기준 split
-    // 소수 판별
+
 func solution(_ n:Int, _ k:Int) -> Int {
-    let arr = String(n, radix: k).split(separator: "0")
-    return arr.filter { isPrime(Int($0)!) }.count
+    let targets = Array(String(n, radix: k).split(separator: "0"))
+    return targets.filter { isPrime(Int($0)!) }.count
 }
 
 func isPrime(_ num: Int) -> Bool {
-    guard num > 1 else { return false }
-
+    guard num != 1 else { return false }
     let end = Int(sqrt(Double(num))) + 1
     
     for i in 2..<end {
