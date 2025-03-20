657c2bb3d853ffcb8035b61b19c7e37ac9ffd955
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-03-20 19:46:56 +0900

    [level 2] Title: 택배상자, Time: 397.88 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/README.md
+++ 프로그래머스/2/131704. 택배상자/README.md
@@ -1,10 +1,10 @@
 # [level 2] 택배상자 - 131704 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131704) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131704#) 
 
 ### 성능 요약
 
-메모리: 60.6 MB, 시간: 233.09 ms
+메모리: 54 MB, 시간: 397.88 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 15일 16:06:23
+2025년 03월 20일 19:46:54
 
 ### 문제 설명
 
--- 프로그래머스/2/131704. 택배상자/택배상자.swift
+++ 프로그래머스/2/131704. 택배상자/택배상자.swift
@@ -1,20 +1,19 @@
 import Foundation
 
 func solution(_ order:[Int]) -> Int {
-    var parcelStack = [Int]()
     var result = 0
-    var headOfBelt = 1
+    var stack = [Int]()
+    var parcel = 1
     
     for o in order {
-        while headOfBelt <= o {     // 스택에 쌓기
-            parcelStack.append(headOfBelt)
-            headOfBelt += 1
-        } // 더 이상 벨트에 o 없음 -> 스택에서 찾으러 가기
+        while parcel <= o {
+            stack.append(parcel)
+            parcel += 1
+        }
 
-        // 스택에서 찾기
-        if parcelStack[parcelStack.count - 1] == o {
-            parcelStack.removeLast()
+        if stack.last! == o {
             result += 1
+            stack.removeLast()
         } else {
             break
         }
