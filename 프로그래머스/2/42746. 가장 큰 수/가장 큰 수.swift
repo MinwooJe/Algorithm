2435743c0b139ec2a09699246041a782ab8988dc
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-03-21 23:04:02 +0900

    [level 2] Title: 가장 큰 수, Time: 670.53 ms, Memory: 22.7 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.swift
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 func solution(_ numbers:[Int]) -> String {
-    let result = numbers
-        .map { String($0)}
+    var result = numbers
+        .map { String($0) }
         .sorted { String(repeating: $0, count: 3) > String(repeating: $1, count: 3) }
         .joined(separator: "")
     
