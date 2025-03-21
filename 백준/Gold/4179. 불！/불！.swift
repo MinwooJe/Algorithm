da411400131cfd802e318c8c6d997e4b05160def
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-03-21 19:14:45 +0900

    [Gold III] Title: 불!, Time: 240 ms, Memory: 131416 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.swift
+++ 백준/Gold/4179. 불！/불！.swift
@@ -1,86 +1,83 @@
-/**
- 지훈 탈출 기준: row == 0 || col == 0  -> 1초 뒤 탈출 성공
- 지훈 bfs, 불 bfs 해야 됨.
- 
- */
-
+// 서로 다른 bfs를 돌려야 하는 문제 -> 하나를 먼저 돌리자
+// 불이 한 군데에서만 난다는 보장이 없음.
 let input = readLine()!.split(separator: " ").map { Int($0)! }
 let (rowCount, colCount) = (input[0], input[1])
 let dRow = [-1, 1, 0, 0]
 let dCol = [0, 0, -1, 1]
 
 var board = [[String]]()
-var fireReached = Array(repeating: Array(repeating: -1, count: colCount), count: rowCount)
+var fireVisited = Array(repeating: Array(repeating: -1, count: colCount), count: rowCount)
 var playerVisited = Array(repeating: Array(repeating: -1, count: colCount), count: rowCount)
-
 var fireQueue = [(Int, Int, Int)]()
 var playerQueue = [(Int, Int, Int)]()
 
-for _ in 0..<rowCount {
+for row in 0..<rowCount {
     let input = readLine()!.map { String($0) }
     board.append(input)
-}
-
-for row in 0..<rowCount {
     for col in 0..<colCount {
-        if board[row][col] == "F" {
-            fireQueue.append((row, col, 0))
-        } else if board[row][col] == "J" {
+        if input[col] == "J" {
             playerQueue.append((row, col, 0))
+            fireVisited[row][col] = 0
+        } else if input[col] == "F" {
+            fireQueue.append((row, col, 0))
+            fireVisited[row][col] = 0
         }
     }
 }
 
-// 불 bfs
-var fireQueueIdx = 0
-while fireQueueIdx < fireQueue.count {
-    let (currRow, currCol, time) = fireQueue[fireQueueIdx]
-    fireReached[currRow][currCol] = time
-    fireQueueIdx += 1
-    
-    for i in 0..<4 {
-        let nextRow = currRow + dRow[i]
-        let nextCol = currCol + dCol[i]
-        
-        guard nextRow >= 0 && nextRow < rowCount && nextCol >= 0 && nextCol < colCount else { continue }
-        guard fireReached[nextRow][nextCol] < 0 && board[nextRow][nextCol] != "#" else { continue }
-        
-        fireQueue.append((nextRow, nextCol, time + 1))
-        fireReached[nextRow][nextCol] = time + 1
-    }
-}
-fireReached.forEach { $0 }
+func fireBfs() {
+    var queueIdx = 0
 
-var playerQueueIdx = 0
-var isSuccess = false
-var escapeTime = 0
-while playerQueueIdx < playerQueue.count {
-    let (currRow, currCol, time) = playerQueue[playerQueueIdx]
-    playerVisited[currRow][currCol] = time
-    playerQueueIdx += 1
-    
-    if currRow == 0 || currRow == rowCount - 1 || currCol == 0 || currCol == colCount - 1 {
-        isSuccess = true
-        escapeTime = time + 1
-        break
-    }
-
-    for i in 0..<4 {
-        let nextRow = currRow + dRow[i]
-        let nextCol = currCol + dCol[i]
+    while queueIdx < fireQueue.count {
+        let (curRow, curCol, time) = fireQueue[queueIdx]
+        queueIdx += 1
         
-        guard nextRow >= 0 && nextRow < rowCount && nextCol >= 0 && nextCol < colCount else { continue }
-        guard playerVisited[nextRow][nextCol] < 0 && board[nextRow][nextCol] != "#" else { continue }
-        // 불이 고립되어 퍼지지 않는 경우에는 fireReached가 모두 -1 이라서 time + 1 < fireReached 비교는 항상 거짓이 됨.
-        guard fireReached[nextRow][nextCol] == -1 || time + 1 < fireReached[nextRow][nextCol] else { continue }
-
-        playerQueue.append((nextRow, nextCol, time + 1))
-        playerVisited[nextRow][nextCol] = time + 1
+        for i in 0..<4 {
+            let nextRow = curRow + dRow[i]
+            let nextCol = curCol + dCol[i]
+            
+            guard nextRow >= 0 && nextRow < rowCount && nextCol >= 0 && nextCol < colCount else { continue }
+            guard board[nextRow][nextCol] != "#" && fireVisited[nextRow][nextCol] == -1 else { continue }
+            fireQueue.append((nextRow, nextCol, time + 1))
+            fireVisited[nextRow][nextCol] = time + 1
+        }
     }
 }
 
-if isSuccess {
-    print(escapeTime)
-} else {
+func playerBfs() -> Int {
+    var escapeTime = -1
+    var queueIdx = 0
+
+    while queueIdx < playerQueue.count {
+        let (curRow, curCol, time) = playerQueue[queueIdx]
+        queueIdx += 1
+        
+        if curRow == 0 || curRow == rowCount - 1 || curCol == 0 || curCol == colCount - 1 {
+            escapeTime = time + 1
+            break
+        }
+        
+        for i in 0..<4 {
+            let nextRow = curRow + dRow[i]
+            let nextCol = curCol + dCol[i]
+            
+            guard nextRow >= 0 && nextRow < rowCount && nextCol >= 0 && nextCol < colCount else { continue }
+            guard board[nextRow][nextCol] != "#" && playerVisited[nextRow][nextCol] == -1 else { continue }
+            guard fireVisited[nextRow][nextCol] == -1 || time + 1 < fireVisited[nextRow][nextCol] else { continue }
+            playerQueue.append((nextRow, nextCol, time + 1))
+            playerVisited[nextRow][nextCol] = time + 1
+        }
+    }
+    
+    return escapeTime
+}
+
+fireBfs()
+
+let escapeTime = playerBfs()
+
+if escapeTime == -1 {
     print("IMPOSSIBLE")
+} else {
+    print(escapeTime)
 }
