a6c468733229d636e97c67cefa135a29ddb8d9b0
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-03-21 18:18:44 +0900

    [Silver I] Title: 그림, Time: 60 ms, Memory: 77620 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.swift
+++ 백준/Silver/1926. 그림/그림.swift
@@ -7,46 +7,47 @@ for _ in 0..<rowCount {
     board.append(input)
 }
 
-var count = 0
-var maxSize = 0
-var visited = Array(repeating: Array(repeating: false, count: colCount), count: rowCount)
 let dRow = [-1, 1, 0, 0]
 let dCol = [0, 0, -1, 1]
+var visited = Array(repeating: Array(repeating: false, count: colCount), count: rowCount)
+var maxSize = 0
+var count = 0
+
 
 for row in 0..<rowCount {
     for col in 0..<colCount {
-        if board[row][col] == 1 && !visited[row][col] {
-            let size = bfs(row, col)
-            count += 1
-            maxSize = max(size, maxSize)
-        }
+        guard board[row][col] == 1 && visited[row][col] == false else { continue }
+        let size = bfs(row, col)
+        count += 1
+        maxSize = max(maxSize, size)
     }
 }
 
 func bfs(_ row: Int, _ col: Int) -> Int {
-    var size = 1            // 시작점을 포함하기 위해
+    var size = 1
     var queue = [(Int, Int)]()
     queue.append((row, col))
+    visited[row][col] = true
+    
+    var queueIdx = 0
+    while queueIdx < queue.count {
+        let (curRow, curCol) = queue[queueIdx]
+        queueIdx += 1
 
-    while !queue.isEmpty {
-        let (curRow, curCol) = queue.removeFirst()
-        visited[curRow][curCol] = true
-        
         for i in 0..<4 {
             let nextRow = curRow + dRow[i]
             let nextCol = curCol + dCol[i]
             
-            if nextRow >= 0 && nextRow < rowCount && nextCol >= 0 && nextCol < colCount {
-                if board[nextRow][nextCol] == 1 && visited[nextRow][nextCol] == false {
-                    queue.append( (nextRow, nextCol) )
-                    visited[nextRow][nextCol] = true
-                    size += 1
-                }
-            }
+            guard nextRow >= 0 && nextRow < rowCount && nextCol >= 0 && nextCol < colCount else { continue }
+            guard board[nextRow][nextCol] == 1 && visited[nextRow][nextCol] == false else { continue }
+            queue.append((nextRow, nextCol))
+            visited[nextRow][nextCol] = true
+            size += 1
         }
     }
-
+    
     return size
 }
 
-print(count, maxSize)
+print(count)
+print(maxSize)
