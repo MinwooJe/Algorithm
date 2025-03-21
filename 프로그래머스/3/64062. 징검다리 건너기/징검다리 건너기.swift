b3ee022dc09fc625386287a14fd7a5f0d32c027d
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-03-21 23:29:09 +0900

    [level 3] Title: 징검다리 건너기, Time: 49.96 ms, Memory: 25.5 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.swift
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.swift
@@ -1,13 +1,19 @@
 import Foundation
 
+/// k: 건너 뛸 수 있는 최대 -> 0인 징검다리가 k개면 못건넘.
 func solution(_ stones:[Int], _ k:Int) -> Int {
-    var left = 1
-    var right = 200000000
+    var result = 0
+    var (l, r) = (0, 200000000)
     
-    while left <= right {
+    // var num = 200000000
+    // while num >= 1 {
+    //     print(num)
+    //     num /= 2
+    // }
+    while l <= r {
+        let mid = (l + r) / 2
         var contiZeroCount = 0
-        let mid = (left + right) / 2
-
+        
         for i in 0..<stones.count {
             if stones[i] - mid <= 0 {
                 contiZeroCount += 1
@@ -19,12 +25,17 @@ func solution(_ stones:[Int], _ k:Int) -> Int {
                 break
             }
         }
-        if contiZeroCount >= k {
-            right = mid - 1
+        
+        if contiZeroCount > k {
+            r = mid - 1
+        } else if contiZeroCount < k {
+            l = mid + 1
         } else {
-            left = mid + 1
+            // contiZeroCount == k라는 말은, 딱 얘네까지 건널 수 있고 이후에는 못건넌다는 말.
+            result = mid
+            r = mid - 1
         }
     }
     
-    return left
+    return result
 }
