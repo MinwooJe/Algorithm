ae7b9d715f56e4a27faf6d2e508f208ef02aeee8
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-03-22 09:46:11 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 308 ms, Memory: 58924 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -3,25 +3,22 @@ import sys, heapq
 
 input = sys.stdin.readline
 INF = int(1e9)
+cityCount = int(input())
+busCount = int(input())
 
-# 도시: 정점, 버스: 간선
-city_count = int(input())
-bus_count = int(input())
 graph = defaultdict(list)
-
-for _ in range(bus_count):
-    city, bus, cost = map(int, input().split())
-    graph[city].append((cost, bus))
-
-
-start_city, end_city = map(int, input().split())
-
-dist = [INF] * (city_count + 1)
-prev_city = [-1] * (city_count + 1)
+dist = [INF] * (cityCount + 1)
+prev = [-1] * (cityCount + 1)
 pq = []
 
-dist[start_city] = 0
-heapq.heappush(pq, (0, start_city))
+for _ in range(busCount):
+    src, des, cost = map(int, input().split())
+    graph[src].append((cost, des))
+    
+start, target = map(int, input().split())
+
+heapq.heappush(pq, (0, start))
+dist[start] = 0
 
 while pq:
     cur_d, cur_v = heapq.heappop(pq)
@@ -30,18 +27,18 @@ while pq:
     
     for next_d, next_v in graph[cur_v]:
         total_d = cur_d + next_d
-        if dist[next_v] <= total_d: continue
+        if total_d >= dist[next_v]: continue
         heapq.heappush(pq, (total_d, next_v))
         dist[next_v] = total_d
-        prev_city[next_v] = cur_v
-
-path = [end_city]
-now = end_city
-while now != start_city:
-    now = prev_city[now]
+        prev[next_v] = cur_v
+        
+path = [target]
+now = target
+while now != start:
+    now = prev[now]
     path.append(now)
 path.reverse()
 
-print(dist[end_city])
+print(dist[target])
 print(len(path))
 print(" ".join(map(str, path)))
