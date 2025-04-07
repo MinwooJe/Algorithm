c8af4fd4c5ddd2d7696782ee30b782d3e39d173e
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-04-07 11:36:53 +0900

    [level 2] Title: 가장 큰 수, Time: 525.36 ms, Memory: 22.9 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.swift
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.swift
@@ -1,14 +1,21 @@
 import Foundation
+/**
+정수 기준 정렬 -> 10, 6, 2 -> x
+문자열 기준 정렬  -> 6, 2, 10
+- 문제점: 3, 30 비교 시 30 > 3으로 처리됨.
+- 왜?: 길이가 다르다면 더 긴놈이 크다고 처리하기 때문
+    - 길이가 짧은애가 더 유리함. 30 3보단 3 30이 더 크니
+    - 짧다고 다는 아님
+    - 3, 34, 30 -> 34 3 30이 젤큼
+*/
 
 func solution(_ numbers:[Int]) -> String {
-    var result = numbers
+    let sortedNumbers = numbers
         .map { String($0) }
         .sorted { String(repeating: $0, count: 3) > String(repeating: $1, count: 3) }
         .joined(separator: "")
     
-    if Set(result) == ["0"] {
-        return "0"
-    } else {
-        return result
-    }
+    guard Set(sortedNumbers) != ["0"] else { return "0" }
+
+    return sortedNumbers
 }
