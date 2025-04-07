e9392790629ccc83b6c8f463146a2d76827a8d7f
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-04-07 11:21:13 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 1210.68 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.swift
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.swift
@@ -1,22 +1,29 @@
 import Foundation
-// f(x): x보다 크고 x와의 비트가 1개 혹은 2개 다른 수 중 최소
-// 비트의 위치까지 봐야됨.
+/**
+2개 이하로 작은 비트를 찾아야 됨 -> 찾지 말고 해당 숫자에서 두개 이하로 바꾸기
+어떻게 해야 가장 작을까? -> 이진수 짝수는 1의 자리가 무조건 0이므로, +1이 답.
+1 -> 0: 작아짐, 0 -> 1: 커짐 
+=> 큰 자릿수부터 쭉 스캔 -> 가장 작은 자릿수의 0을 1로 바꾸면 됨
+-> 2개까진 달라도 되니, 그다음 자릿수를 그냥 0으로 바꿔버리면 더 작아짐.
+-> if let  = lastIndex(of: "0") 이용해서 찾기
+-> if let =  lastIndex를 이용한다면 swapAt( , ) 이용하면 코드 깔끔해짐.
+-> 이진수 홀수는 1의 자리가 무조건 1이니깐 보장됨.
+*/
 func solution(_ numbers:[Int64]) -> [Int64] {
-    return numbers.map { getFx($0)}
+    return numbers.map { getNextNum($0) }
 }
 
-func getFx(_ num: Int64) -> Int64 {
+func getNextNum(_ num: Int64) -> Int64 {
     if num % 2 == 0 {
         return num + 1
     } else {
         var binaryNum = Array(String(num, radix: 2))
-
+        
         if let lastZeroIdx = binaryNum.lastIndex(of: "0") {
             binaryNum.swapAt(lastZeroIdx, lastZeroIdx + 1)
         } else {
             binaryNum.insert("0", at: 1)
         }
-        
         return Int64(String(binaryNum), radix: 2)!
     }
 }
