66c9e1f0a49398ec1aa9795fd3324f4fedf9de80
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-04-10 13:39:52 +0900

    [Silver III] Title: 01타일, Time: 16 ms, Memory: 76916 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.swift
+++ 백준/Silver/1904. 01타일/01타일.swift
@@ -1,20 +1,17 @@
 let n = Int(readLine()!)!
 
-var memo = [Int: Int]()
-
-func dp(_ n: Int) -> Int {
-    if n == 1 {
-        return 1
-    }
-    if n == 2 {
-        return 2
+func fibonacci(_ n: Int) -> Int {
+    guard n != 1 else { return 1 }
+    guard n != 2 else { return 2 }
+    
+    var table = Array(repeating: 0, count: n + 1)
+    (table[1], table[2]) = (1, 2)
+    
+    for i in 3...n {
+        table[i] = (table[i - 1] + table[i - 2]) % 15746
     }
     
-    if !memo.keys.contains(n) {
-        memo[n] = (dp(n - 1) + dp(n - 2)) % 15746
-    }
-    
-    return memo[n]!
+    return table[n]
 }
 
-print(dp(n))
+print(fibonacci(n))
