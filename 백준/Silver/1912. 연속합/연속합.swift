6f4812adf248542c00299960aa22047c4917e505
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-04-10 14:34:42 +0900

    [Silver II] Title: 연속합, Time: 28 ms, Memory: 76112 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.swift
+++ 백준/Silver/1912. 연속합/연속합.swift
@@ -1,11 +1,10 @@
 let n = Int(readLine()!)!
-let arr = readLine()!.split(separator: " ").map { Int(String($0))! }
 
-var memo = Array(repeating: -1001, count: n)
-memo[0] = arr[0]
+let arr = readLine()!.split(separator: " ").map { Int($0)! }
+var memo: [Int] = [arr[0]]
 
 for i in 1..<n {
-    memo[i] = max(arr[i], arr[i] + memo[i - 1])
+    memo.append(max(arr[i], arr[i] + memo[i - 1]))
 }
 
 print(memo.max()!)
