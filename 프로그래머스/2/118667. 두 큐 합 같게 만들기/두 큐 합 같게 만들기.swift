498a9fce36ad14898c332d9bce87f7034dd567cd
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-04-16 22:03:24 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 66.07 ms, Memory: 42.1 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.swift
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.swift
@@ -1,36 +1,33 @@
 import Foundation
 
-/**
-주의 사항
-- queue가 배열 -> pop 시 오버헤드 존재 -> 인덱스 사용
-    -> 큐 길이가 매번 변함 -> while문의 조건을 위해 처음의 원소 개수 구해야 됨.
-- 큐의 최대 길이 300000 -> 매번 합 계산 오버헤드 존재 -> 합을 처음에만 구하고 갱신 -> sum 변수 필요
-*/
 func solution(_ queue1:[Int], _ queue2:[Int]) -> Int {
-    var (sum1, sum2) = (queue1.reduce(0, +), queue2.reduce(0, +))
+    let maxCount = (queue1.count + queue2.count) * 2
+    var (queue1, queue2) = (queue1, queue2)
+    var (idx1, idx2) = (0, 0)
+    var sum1 = queue1.reduce(0, +)
+    var sum2 = queue2.reduce(0, +)
+    var result = 0
     
     guard (sum1 + sum2) % 2 == 0 else { return -1 }
     
-    var (queue1, queue2) = (queue1, queue2)
-    let (queue1Count, queue2Count) = (queue1.count, queue2.count)
-    var (idx1, idx2) = (0, 0)
-    var result = 0
-    
-    while result < (queue1Count + queue2Count) * 2 {
-        if sum1 < sum2 {
+    while result < maxCount {
+        if sum1 == sum2 {
+            return result
+        } else if sum1 < sum2 {
+            // queue2의 원소 pop -> queue1 append
             queue1.append(queue2[idx2])
             sum1 += queue2[idx2]
             sum2 -= queue2[idx2]
             idx2 += 1
-        } else if sum1 > sum2 {
-            queue2.append(queue1[idx1])
-            sum1 -= queue1[idx1]
-            sum2 += queue1[idx1]
-            idx1 += 1
         } else {
-            return result
+            // queue1의 원소 pop -> queue2 append
+            queue2.append(queue1[idx1])
+            sum2 += queue1[idx1]
+            sum1 -= queue1[idx1]
+            idx1 += 1
         }
         result += 1
     }
+    
     return -1
 }
