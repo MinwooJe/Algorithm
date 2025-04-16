5e1d4ce69465855f310d61f971451e14dd146b01
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-04-16 21:42:21 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 51.70 ms, Memory: 64.5 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.swift
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.swift
@@ -4,32 +4,31 @@ var (zeroCount, oneCount) = (0, 0)
 
 func solution(_ arr:[[Int]]) -> [Int] {
     recursive(arr, 0, 0, arr.count)
-
     return [zeroCount, oneCount]
 }
 
-// 압축 안되는 놈들만 파고 들어가기
-/// 매개변수로 시작점을 받고
+/// row, col: 시작 행, 열 인덱스, n: 탐색할 정사각형 한 변의 길이
 func recursive(_ arr: [[Int]], _ row: Int, _ col: Int, _ n: Int) {
     let element = arr[row][col]
     
-    // 반복문에서 끝지점을 만들기
     for r in row..<row + n {
         for c in col..<col + n {
             if arr[r][c] != element {
+                // 재귀 호출 - 4등분
                 recursive(arr, row, col, n / 2)
                 recursive(arr, row + n / 2, col, n / 2)
                 recursive(arr, row, col + n / 2, n / 2)
                 recursive(arr, row + n / 2, col + n / 2, n / 2)
-                
                 return
             }
         }
     }
     
+    // 해당 구역(재귀 함수의 탐색 범위)이 모두 같은 수라면 더 이상 볼 필요 없음.
     if element == 1 {
         oneCount += 1
     } else {
         zeroCount += 1
     }
+    
 }
