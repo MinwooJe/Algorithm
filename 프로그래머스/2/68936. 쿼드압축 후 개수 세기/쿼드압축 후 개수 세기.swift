25ebbcfc00a3d0d381a321b030b7165f255ad14b
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-04-21 21:43:18 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 52.78 ms, Memory: 28.5 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.swift
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.swift
@@ -1,34 +1,30 @@
 import Foundation
 
-var (zeroCount, oneCount) = (0, 0)
-
 func solution(_ arr:[[Int]]) -> [Int] {
-    recursive(arr, 0, 0, arr.count)
-    return [zeroCount, oneCount]
-}
+    var (zeroCount, oneCount) = (0, 0)
 
-/// row, col: 시작 행, 열 인덱스, n: 탐색할 정사각형 한 변의 길이
-func recursive(_ arr: [[Int]], _ row: Int, _ col: Int, _ n: Int) {
-    let element = arr[row][col]
-    
-    for r in row..<row + n {
-        for c in col..<col + n {
-            if arr[r][c] != element {
-                // 재귀 호출 - 4등분
-                recursive(arr, row, col, n / 2)
-                recursive(arr, row + n / 2, col, n / 2)
-                recursive(arr, row, col + n / 2, n / 2)
-                recursive(arr, row + n / 2, col + n / 2, n / 2)
-                return
+    func bruteforce(_ arr: [[Int]], _ startRow: Int, _ startCol: Int, _ length: Int) {
+        let element = arr[startRow][startCol]
+
+        for row in startRow..<startRow + length {
+            for col in startCol..<startCol + length {
+                if arr[row][col] != element {
+                    bruteforce(arr, startRow, startCol, length / 2)
+                    bruteforce(arr, startRow, startCol + length / 2, length / 2)
+                    bruteforce(arr, startRow + length / 2, startCol, length / 2)
+                    bruteforce(arr, startRow + length / 2, startCol + length / 2, length / 2)
+                    return
+                }
             }
+        }
+        
+        if element == 1 {
+            oneCount += 1
+        } else {
+            zeroCount += 1
         }
     }
     
-    // 해당 구역(재귀 함수의 탐색 범위)이 모두 같은 수라면 더 이상 볼 필요 없음.
-    if element == 1 {
-        oneCount += 1
-    } else {
-        zeroCount += 1
-    }
-    
+    bruteforce(arr, 0, 0, arr.count)
+    return [zeroCount, oneCount]
 }
