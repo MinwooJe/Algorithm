efc6c723652c24501a364dd8d969569edc88aed9
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-05-22 15:28:39 +0900

    [level 2] Title: 피로도, Time: 77.01 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946#) 
 
 ### 성능 요약
 
-메모리: 16.5 MB, 시간: 243.75 ms
+메모리: 16.2 MB, 시간: 77.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 21일 16:30:27
+2025년 05월 22일 15:28:37
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.swift
+++ 프로그래머스/2/87946. 피로도/피로도.swift
@@ -1,20 +1,26 @@
 import Foundation
 
+/**
+완탐 이유: 최소 피로도가 낮은 순으로 해도 최적해 보장 x, 소모 피로도 순으로 해도 최적해 보장 x
+-> 둘 다 따져보면서 순서를 정해야하므로 백트래킹을 하면 됨.
+- 시간 복잡도: 8! = 720 * 72 이므로 충분
+*/
 func solution(_ k:Int, _ dungeons:[[Int]]) -> Int {
     var result = 0
-    
-    func backtrack(_ currHp: Int, _ curr: [Int]) {
-        result = max(result, curr.count)
+
+    func backtrack(_ hp: Int, _ visited: Set<Int>) {
+        var visited = visited
+        result = max(visited.count, result)
         
         for i in 0..<dungeons.count {
-            let dungeon = dungeons[i]
-            if !curr.contains(i) && currHp >= dungeon[0] {
-                backtrack(currHp - dungeon[1], curr + [i])
+            if !visited.contains(i) && hp >= dungeons[i][0]{
+                visited.insert(i)
+                backtrack(hp - dungeons[i][1], visited)
+                visited.remove(i)
             }
         }
     }
-    
+
     backtrack(k, [])
-    
     return result
 }
