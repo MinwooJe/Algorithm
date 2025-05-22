1d1d9d16e4e4b87da0248cf7e7751ddaf232f9a9
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-05-22 12:42:59 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.swift
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.swift
@@ -1,26 +1,18 @@
 import Foundation
-/**
-- 중복 단어 x
-- 앞 사람의 마지막 글자 == 뒷 사람의 첫 글자
 
-출력: [가장 먼저 탈락하는 사람의 번호, 자신의 몇 번째 차례인지?]
-*/
 func solution(_ n:Int, _ words:[String]) -> [Int] {
-    var visitedWords = Set<String>()
+    var visited = Set<String>()
     var result = [0, 0]
-
-    visitedWords.insert(words[0])
+    
+    visited.insert(words[0])
     for i in 1..<words.count {
-        if words[i - 1].last! != words[i].first! || visitedWords.contains(words[i]) {
-            print(words[i], i)
-            let player = i % n + 1
-            let turn = i / n + 1
-            result = [player, turn]
+        if words[i - 1].last! != words[i].first! || visited.contains(words[i]) {
+            result = [i % n + 1, i / n + 1]
             break
         }
-        visitedWords.insert(words[i])
-        print(visitedWords)
-    }
 
+        visited.insert(words[i])
+    }
+    
     return result
 }
