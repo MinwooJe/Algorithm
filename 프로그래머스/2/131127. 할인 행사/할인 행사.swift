6a9734bcb48d91abc043e85e1b7ee383091a5b77
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-05-22 14:00:53 +0900

    [level 2] Title: 할인 행사, Time: 82.46 ms, Memory: 21.5 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.swift
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.swift
@@ -1,18 +1,32 @@
 import Foundation
 
+/**
+1. 정현이 딕셔너리 만들기
+2. 할인 딕셔너리 만들기
+- 날짜 별 할인 품목 갱신
+- 정현이 딕셔너리랑 같은지 확인
+*/
 func solution(_ want:[String], _ number:[Int], _ discount:[String]) -> Int {
-    var wantDict = Dictionary(uniqueKeysWithValues: zip(want, number))
+    let wantDict = Dictionary(uniqueKeysWithValues: zip(want, number))
+    var discountDict = [String: Int]()
     var result = 0
     
-    for startIdx in 0...discount.count - 10 {
-        var marketDict = [String: Int]()
-
-        for i in startIdx..<startIdx + 10 {
-            marketDict[discount[i], default: 0] += 1
-        }
-
-        result += wantDict == marketDict ? 1 : 0
+    // discountDict 초기화
+    for i in 0..<10 {
+        discountDict[discount[i], default: 0] += 1
     }
+    
+    
+    for i in 0..<discount.count - 10 {
+        result += wantDict == discountDict ? 1 : 0
+        discountDict[discount[i]]! -= 1
+        discountDict[discount[i + 10], default: 0] += 1
+        
+        if discountDict[discount[i]]! == 0 {
+            discountDict[discount[i]] = nil
+        }
+    }
+    result += wantDict == discountDict ? 1 : 0
     
     return result
 }
