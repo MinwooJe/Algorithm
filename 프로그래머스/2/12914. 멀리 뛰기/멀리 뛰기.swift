0351d5ffbd0fd9938779f33c8cb910515ec55907
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-05-22 11:14:42 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.06 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.swift
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.swift
@@ -1,15 +1,17 @@
 func solution(_ n:Int) -> Int {
-    var dp = Array(repeating: -1, count: n + 1)
+    var table = Array(repeating: 0, count: n + 1)
+    
     if n == 1 {
         return 1
     } else if n == 2 {
         return 2
     }
+
+    (table[1], table[2]) = (1, 2)
     
-    (dp[1], dp[2]) = (1, 2)
     for i in 3...n {
-        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567
+        table[i] = (table[i - 1] + table[i - 2]) % 1234567
     }
 
-    return dp[n]
+    return table[n]
 }
