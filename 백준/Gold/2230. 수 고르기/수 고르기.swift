4931c7275881b5ca4c8f662cde19a2574c916f56
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-05-24 14:29:30 +0900

    [Gold V] Title: 수 고르기, Time: 60 ms, Memory: 70676 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.swift
+++ 백준/Gold/2230. 수 고르기/수 고르기.swift
@@ -1,24 +1,29 @@
-// n: 배열 개수, m: 차이가 m 이상이면서 제일 작은수
+/**
+ 가정: 배열이 오름차순으로 정렬됨.
+ 1. i가 증가함에 따라 a[j] - a[i]가 m 이상이 되는 최초의 지점 j 또한 증가.
+ 2. 각 i에 대해 a[j] - a[i]가 m 이상이 되는 최초의 지점 j를 찾은 이후에는 a[j + 1], a[j + 2], ... 를 확인할 필요가 없음.
+ 이중 for문 완탐과 비교해서, 각 i에 대한 모든 j를 보지않고, 이전의 j값을 참고해서 최적화.
+ 시간 복잡도: l, r이 배열을 한 번 순회 -> O(n)
+ */
 let input = readLine()!.split(separator: " ").map { Int($0)! }
 let (n, m) = (input[0], input[1])
 var arr = [Int]()
+var result = Int.max
 
 for _ in 0..<n {
     let input = Int(readLine()!)!
     arr.append(input)
 }
 
-arr = arr.sorted()
+arr.sort()
 
 var (l, r) = (0, 0)
-var result = Int.max
-
-while l < arr.count && r < arr.count && l <= r {
+while r < arr.count && l < arr.count {
     let diff = arr[r] - arr[l]
     
     if diff >= m {
-        result = min(diff, result)
         l += 1
+        result = min(result, diff)
     } else {
         r += 1
     }
