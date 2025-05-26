7e3f1e14823b81ae31a0fee4c9745c05611dc000
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-05-26 17:45:59 +0900

    [Gold III] Title: 소수의 연속합, Time: 40 ms, Memory: 89664 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.swift
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.swift
@@ -1,48 +1,41 @@
 import Foundation
 
-// 소수 중 연속.
-let target = Int(readLine()!)!
-var primes = getPrimes(target)
-var r = 0
+let n = Int(readLine()!)!
+let primes = getPrimes(n)
 var result = 0
 
-if !primes.isEmpty {
+if primes.isEmpty {
+    result = 0
+} else {
+    var r = 0
     var sum = primes[0]
-    
+
     for l in 0..<primes.count {
-        while sum < target && r < primes.count - 1 {
+        while sum < n && r < primes.count - 1 {
             r += 1
             sum += primes[r]
         }
         
-        if sum == target {
+        if sum == n {
             result += 1
         }
-        
         sum -= primes[l]
     }
 }
+
 print(result)
 
-// target 이하의 소수 만들기
-func getPrimes(_ target: Int) -> [Int] {
-    let end = Int(sqrt(Double(target)))
-    var nums = Array(repeating: true, count: target + 1)
-    
-    for i in 2..<end + 1 {
-        guard nums[i] else { continue }
-        for j in stride(from: i + i, to: target + 1, by: i) {
-            nums[j] = false
-        }
-    }
-    
-    var result = [Int]()
-    for (idx, value) in nums.enumerated() {
-        if value && idx >= 2 {
-            result.append(idx)
-        }
-    }
-    
-    return result
-}
+func getPrimes(_ n: Int) -> [Int] {
+    guard n >= 2 else { return [] }
+    var isPrime = Array(repeating: true, count: n + 1)
+    let end = Int(sqrt(Double(n)))
 
+    for i in 2..<end + 1 {
+        guard isPrime[i] else { continue }
+        for j in stride(from: i + i, through: n, by: i) {
+            isPrime[j] = false
+        }
+    }
+    
+    return (2...n).filter { isPrime[$0] }
+}
