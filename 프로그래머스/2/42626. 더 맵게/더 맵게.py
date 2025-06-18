3e4b8021fc8a74bd0c90375d398b541a840b00af
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2025-06-18 14:16:25 +0900

    [level 2] Title: 더 맵게, Time: 1835.04 ms, Memory: 48.9 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,34 +1,22 @@
-import heapq
-"""
-1. 스코빌 배열에서 최솟값 두 개 찾기 -> 둘 다 K를 넘는지 확인
-안넘는다면
-2. 섞기
-3. 삽입
+import heapq as hq
 
-최솟값을 찾는데 비용 큼 -> 힙 사용
-"""
 def solution(scoville, K):
-    isSuccess = False
-    answer = 0
-    foods = scoville
-    heapq.heapify(scoville)
+    hq.heapify(scoville)
+    result = 0
     
-    while len(foods) >= 2 :
-        min1 = heapq.heappop(foods)
-        min2 = heapq.heappop(foods)
+    while len(scoville) >= 2:
+        min1 = hq.heappop(scoville)
+        min2 = hq.heappop(scoville)
         
-        if min1 >= K:
-            isSuccess = True
-            break
+        if min1 >= K: return result
         
-        new = min1 + min2 * 2
-        heapq.heappush(foods, new)
-        answer += 1
-    
-    if foods and heapq.heappop(foods) >= K:
-        isSuccess = True
-    
-    if isSuccess:
-        return answer
+        mixed = min1 + min2 * 2
+        hq.heappush(scoville, mixed)
+        result += 1
+        
+    if scoville and hq.heappop(scoville) >= K:
+        return result
     else:
         return -1
+        
+        
